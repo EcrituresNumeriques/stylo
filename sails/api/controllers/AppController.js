d3e7d23eac22e055a1cb4bafbdf15f7411d3a9b8
Arthur Juchereau <arthur.juchereau@gmail.com>
2017-10-08 00:01:32 -0400

    prod ready

--- sails/api/controllers/AppController.js
+++ sails/api/controllers/AppController.js
@@ -1,7 +1,6 @@
 module.exports = {
   index: function (req, res) {
-    let prod = true;
-    if(prod){
+    if(process.env.NODE_ENV == "production"){
       return res.sendfile(sails.config.appPath + '/dist/index.html');
     }
     else{
