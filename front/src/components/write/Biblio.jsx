8df9ed1155aa892471cf57ac5bbd26fd482f16ed
PookMook <arthur.juchereau@gmail.com>
2018-04-27 13:30:29 -0400

    biblio

--- front/src/components/write/Biblio.jsx
+++ front/src/components/write/Biblio.jsx
@@ -5,7 +5,14 @@ export default function Biblio(props){
   return (
     <div id="biblio">
         <h1>Biblio</h1>
-        <pre>{props.bib}</pre>
+        {props.bib.split('\n').map(function(line,i){
+            if(line.match(/^@+/)){
+                return (<pre key={"line"+i}>{line}</pre>)
+            }
+            else{
+                return null
+            }
+        })}
     </div>
   )
 }
