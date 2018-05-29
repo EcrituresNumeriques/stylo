fd75753ed593bd1b4067bd0f456953e619b268cc
Arthur Juchereau <PookMook@users.noreply.github.com>
2018-05-29 16:06:56 -0400

    Update Users.js

--- sails/api/models/Users.js
+++ sails/api/models/Users.js
@@ -4,6 +4,11 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/#!documentation/models
  */
+const defaultArticle = {
+  md:"# titre",
+  bib:"",
+  yaml:"---\ntitle:Title\n---"
+};
 
 module.exports = {
   tableName: 'users',
