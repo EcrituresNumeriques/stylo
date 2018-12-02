5411c393ababc536b897b29c5a9736b51ad007a4
PookMook <arthur.juchereau@gmail.com>
2018-12-02 13:11:07 -0500

    full bibtex tooltip

--- front/src/components/write/Biblio.jsx
+++ front/src/components/write/Biblio.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 import Clipboard from 'react-clipboard.js';
 
 function compare(a,b) {
-  if (a < b)
+  if (a.key < b.key)
     return -1;
-  if (a > b)
+  if (a.key > b.key)
     return 1;
   return 0;
 }
@@ -13,13 +13,17 @@ function compare(a,b) {
 export default function Biblio(props){
     const bib = props.bib || "";
     let entries = [];
-    entries = bib.split('\n').map((line)=>(line.match(/^@.+{(.+),/))).filter(line=>line!=null).map(line=>line[1]).sort(compare);
+    entries = bib.split(/(?=@)/g)
+      .filter((ref)=>(ref.match(/^@/g)))
+      .map((ref)=>(ref.replace(/^\s+|\s+$/g, '')))
+      .map((ref)=>({title:ref.replace(/\n/g,'&#13;'),key:ref.match(/^@.+{(.+),/)[1]}))
+      .sort(compare);
 
   return (
     <div id="biblio">
         <h1 className={props.closed?"title closed":"title"} onDoubleClick={()=>props.toggle()}>Biblio</h1>
         {!props.closed && <section>
-            {entries.map((ref,i)=>(<Clipboard key={"ref"+i} component="p" data-clipboard-text={"[@"+ref+"]"} button-title={"[@"+ref+"]"}>@{ref}</Clipboard>))}
+            {entries.map((ref,i)=>(<Clipboard key={"ref"+i} component="p" data-clipboard-text={"[@"+ref.key+"]"} button-title={"[@"+ref.title+"]"}>@{ref.key}</Clipboard>))}
         </section>}
         {props.addRef && !props.closed && <button onClick={()=>props.addRef()}>+ Add reference</button>}
         {props.sourceRef && !props.closed && <button onClick={()=>props.sourceRef()}>See source</button>}
