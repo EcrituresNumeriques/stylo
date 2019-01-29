35b1692bbe4955cb79550bb42619b48fc2b38258
PookMook <arthur.juchereau@gmail.com>
2019-01-29 14:28:02 -0500

    add People component

--- front/src/components/yamleditor/Authors.jsx
+++ front/src/components/yamleditor/Authors.jsx
@@ -1,4 +1,4 @@
-import { TextInput } from './TextInput.jsx';
+import { People } from './People.jsx'
 import React from 'react';
 import _ from 'lodash';
 
@@ -8,22 +8,8 @@ export function Authors(props){
   return(
     <section>
       <h1><i className="fa fa-users" aria-hidden="true"></i> Auteurs</h1>
-      {authors.map((o,i)=>(<Author key={i} index={i} state={props.state} updateState={props.updateState} readOnly={props.readOnly}/>))}
+      {authors.map((o,i)=>(<People key={i} index={i} keyState="authors" state={props.state} updateState={props.updateState} readOnly={props.readOnly}/>))}
       {!props.readOnly && <p className="addToArray" onClick={function(){props.updateState({"forname": "","surname": "","orcid": "","viaf": "","foaf": "","isni": "","wikidata": ""},"authors["+targetNewAuthor+"]")}}><i className="fa fa-user-plus" aria-hidden="true"></i> Ajouter un auteur</p>}
-    </section>
-  )
-}
-
-function Author(props){
-  return(
-    <section className="group">
-      <TextInput target={"authors["+props.index+"].surname"} title="Nom" state={props.state} updateState={props.updateState} readOnly={props.readOnly}/>
-      <TextInput target={"authors["+props.index+"].forname"} title="Prénom" state={props.state} updateState={props.updateState} readOnly={props.readOnly}/>
-      <TextInput target={"authors["+props.index+"].orcid"} title="Orcid" state={props.state} updateState={props.updateState} readOnly={props.readOnly}/>
-      <TextInput target={"authors["+props.index+"].viaf"} title="VIAF" state={props.state} updateState={props.updateState} readOnly={props.readOnly}/>
-      <TextInput target={"authors["+props.index+"].foaf"} title="FOAF" state={props.state} updateState={props.updateState} readOnly={props.readOnly}/>
-      <TextInput target={"authors["+props.index+"].isni"} title="ISNI" state={props.state} updateState={props.updateState} readOnly={props.readOnly}/>
-      <TextInput target={"authors["+props.index+"].wikidata"} title="Wikidata" state={props.state} updateState={props.updateState} readOnly={props.readOnly}/>
     </section>
   )
 }
