123474669a9e50030ff2180f46373193347f22d6
PookMook <arthur.juchereau@gmail.com>
2019-03-05 16:47:42 -0500

    context switcher for Yamleditor

--- front/gatsby/src/components/Write/WriteRight.js
+++ front/gatsby/src/components/Write/WriteRight.js
@@ -3,9 +3,12 @@ import React, {useState} from 'react'
 import styles from '../write.module.scss'
 import YamlEditor from './yamleditor/YamlEditor'
 
+import etv from '../../helpers/eventTargetValue'
+
 export default (props) => {
 
   const [expanded,setExpanded] = useState(false)
+  const [selector, setSelector] = useState('basic')
  
   return (
     <nav className={`${expanded?styles.expandRight:styles.retractRight}`}>
@@ -15,8 +18,14 @@ export default (props) => {
           <header>
             <h1>Metadata</h1>
           </header>
-          {props.readOnly && <YamlEditor yaml={props.yaml}/>}
-          {!props.readOnly && <YamlEditor yaml={props.yaml} exportChange={(change)=>props.handleYaml(change)}/>}
+          <nav>
+            <p onClick={(e)=>setSelector('basic')}>Basic Mode [{selector==="basic"?'X':' '}]</p>
+            <p onClick={(e)=>setSelector('editor')}>Editor Mode [{selector==="editor"?'X':' '}]</p>
+            <p onClick={(e)=>setSelector('raw')}>Raw Mode [{selector==="raw"?'X':' '}]</p>
+          </nav>
+          {selector === "raw" && <textarea value={props.yaml} onChange={(e)=>props.handleYaml(etv(e))}/>}
+          {selector !== "raw" && props.readOnly && <YamlEditor yaml={props.yaml} editor={selector === "editor"}/>}
+          {selector !== "raw" && !props.readOnly && <YamlEditor yaml={props.yaml} exportChange={(change)=>props.handleYaml(change)} editor={selector === "editor"}/>}
 
         </div>
         </>}
