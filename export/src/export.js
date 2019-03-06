a5845f108b134bd1addc59260bdf59156fb85e71
PookMook <arthur.juchereau@gmail.com>
2019-03-06 17:07:57 -0500

    export article+version .zip

--- export/src/export.js
+++ export/src/export.js
@@ -20,11 +20,23 @@ const exportHTML = ({bib,yaml,md,id,title},res,req) => {
     return res.status(500).send(`${html5}`)
   }
   if(!req.query.preview){
-    res.set('Content-Disposition', `attachment; filename="${title}v.html"`)
+    res.set('Content-Disposition', `attachment; filename="${title}.html"`)
   }
   const html5 = shell.cat(`${id}.html`)
   return res.send(`${html5}`)
-  }
+}
+
+const exportZIP = ({bib,yaml,md,id,title},res,req) => {
+  shell.cd('src/data')
+  shell.exec(`rm ${id}*`)
+  shell.echo(md).to(`${id}.md`)
+  shell.echo(bib).to(`${id}.bib`)
+  shell.echo(yaml).to(`${id}.yaml`)
+  shell.exec(`zip ${title}.zip ${id}.*`)
+  res.set('Content-Disposition', `attachment; filename="${title}.zip"`)
+  return res.download(`${process.env.PWD}/src/data/${title}.zip`)
+}
+
 
 module.exports = {
   exportArticleHtml: async (req,res,next)=>{
@@ -63,11 +75,40 @@ module.exports = {
     } 
   
   },
-  exportArticleZip: (req,res,next)=>{
-    res.send(`<p>Exporting Article ${req.params.id} to ZIP</p>`)
+  exportArticleZip: async (req,res,next)=>{
+    try{
+      const article = await Article.findById(req.params.id)
+      if(!article){
+        throw new Error('Article Not found')
+      }
+      const versionID = article._doc.versions[article._doc.versions.length-1]
+      const version = await Version.findById(versionID)
+      if(!version){
+        throw new Error('Version not found')
+      }
+      const cleanedVersion = version._doc
+
+      exportZIP({bib:cleanedVersion.bib,yaml:cleanedVersion.yaml,md:cleanedVersion.md, id:cleanedVersion._id, title:article._doc.title}, res, req)
+
+    }
+    catch(err){
+      res.status(404).send(err)
+    }
   },
-  exportVersionZip: (req,res,next)=>{
-    res.send(`<p>Exporting Version ${req.params.id} to ZIP</p>`)
+  exportVersionZip: async (req,res,next)=>{
+    try{
+      const version = await Version.findById(req.params.id)
+       if(!version){
+         throw new Error('Version not found')
+       }
+       const cleanedVersion = version._doc
+ 
+       exportZIP({bib:cleanedVersion.bib,yaml:cleanedVersion.yaml,md:cleanedVersion.md, id:cleanedVersion._id, title:cleanedVersion._id}, res, req)
+ 
+     }
+     catch(err){
+       res.status(404).send(err)
+     } 
   },
   exportBookHtml: (req,res,next)=>{
     res.send(`<p>Exporting Book ${req.params.id} to HTML</p>`)
