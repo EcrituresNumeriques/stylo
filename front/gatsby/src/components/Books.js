ab16d4e4376bbde7a7a40185ea0daaa268a943de
PookMook <arthur.juchereau@gmail.com>
2019-03-07 18:19:14 -0500

    first bases for books

--- front/gatsby/src/components/Books.js
+++ front/gatsby/src/components/Books.js
@@ -19,9 +19,38 @@ const ConnectedBooks = (props) => {
     }
 
     const [isLoading,setIsLoading] = useState(true)
+    const [displayName,setDisplayName] = useState(props.users[0].displayName)
+    const [tags,setTags] = useState([])
+    const [needReload,setNeedReload] = useState(true)
+
+    useEffect(()=>{
+        if(needReload){
+            //Self invoking async function
+            (async () =>{
+                try{
+                    const query = `query($user:ID!){user(user:$user){ displayName tags{ _id name  bib yaml articles{ _id title updatedAt versions(limit:1){ _id version revision autosave message } } } } }`
+                    const user = {user:props.users[0]._id}
+                    setIsLoading(true)
+                    const data = await askGraphQL({query,variables:user},'fetching articles',props.sessionToken)
+                    //Need to sort by updatedAt desc
+                    setTags(data.user.tags)
+                    setDisplayName(data.user.displayName)
+                    setIsLoading(false)
+                    setNeedReload(false)
+                }
+                catch(err){
+                    alert(err)
+                }
+            })()
+        }
+    },[needReload])
     
     return (
         <section className={styles.section}>
+            <h1>Books for {displayName}</h1>
+            <p>Books are like super-tags, they are a collection of articles that you can ordonnate and export all at once</p>
+            <p>Below are your tags eligible for being books:</p>
+
         </section>
     )
 }
