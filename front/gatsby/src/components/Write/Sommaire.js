91a54cc09862ae7289789805acecd9ba02dbcac7
PookMook <arthur.juchereau@gmail.com>
2019-03-10 11:49:00 -0400

    symbols

--- front/gatsby/src/components/Write/Sommaire.js
+++ front/gatsby/src/components/Write/Sommaire.js
@@ -10,13 +10,22 @@ export default (props) => {
   // eslint-disable-next-line
   const lines = props.md.split('\n').map((l,i)=>{return {line:i,payload:l}}).filter(l=>l.payload.match(/^##+\ /))
 
+  //arrow backspace \u21B3
+  // right arrow \u2192
+  // nbsp \xa0
+  // down arrow \u2223
+  // right tack \u22A2
+  // bottom left box drawing \u2514
+  // left drawing \u2502
+  //23B8
+
   return (
     
     <section>
       <h1 className={expand?null:styles.closed} onDoubleClick={()=>setExpand(!expand)}>Table of content</h1>
       {expand && <>
         <ul>
-          {lines.map((l)=><li key={`line-${l.line}-${l.payload}`} onClick={()=>props.setCodeMirrorCursor(l.line)}>{l.payload.replace(/#/,'').replace(/#\s/g,'\u21B3').replace(/#/g,'\xa0\xa0')}</li>)}
+          {lines.map((l)=><li key={`line-${l.line}-${l.payload}`} onClick={()=>props.setCodeMirrorCursor(l.line)}>{l.payload.replace(/##/,'').replace(/#\s/g,'\u21B3').replace(/#/g,'\u00B7\xa0')}</li>)}
         </ul>
       </>}
     </section>
