938f7beea6e7b17aa8c684d22c461e209f64c0d4
PookMook <arthur.juchereau@gmail.com>
2019-03-13 12:45:03 -0400

    up the file limit to 50Mb for huge texts

--- graphql/app.js
+++ graphql/app.js
@@ -29,7 +29,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit:'50mb'}));
 app.use(cookieParser());
 app.use(isAuth);
 app.use(displayUser);
