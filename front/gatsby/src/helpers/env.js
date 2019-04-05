2faaf15c505181c1bd9a8c2e47964638cb966ce3
PookMook <arthur.juchereau@gmail.com>
2019-04-04 21:13:59 -0400

    ready for prod

--- front/gatsby/src/helpers/env.js
+++ front/gatsby/src/helpers/env.js
@@ -1,7 +1,7 @@
 console.log(process.env.GATSBY_GRAPHQL_ENDPOINT)
 const env = {
-  GRAPHQL_ENDPOINT: process.env.GATSBY_GRAPHQL_ENDPOINT || "https://graphql.stylo.14159.ninja/graphql",
-  EXPORT_ENDPOINT: process.env.GATSBY_EXPORT_ENDPOINT || "https://stylo.14159.ninja",
+  GRAPHQL_ENDPOINT: process.env.GATSBY_GRAPHQL_ENDPOINT || "https://graphql.stylo.ecrituresnumeriques.ca/graphql",
+  EXPORT_ENDPOINT: process.env.GATSBY_EXPORT_ENDPOINT || "https://stylo.ecrituresnumeriques.ca",
   PROCESS_ENDPOINT: process.env.GATSBY_PROCESS_ENDPOINT || "https://stylo-export.ecrituresnumeriques.ca"
 }
 
