adc0f10dd795ec7d76bbfce121ffd48975628398
PookMook <arthur.juchereau@gmail.com>
2019-06-20 15:16:44 -0400

    filter tags

--- front/gatsby/src/components/TagManagement.js
+++ front/gatsby/src/components/TagManagement.js
@@ -3,12 +3,13 @@ import React, {useState} from "react"
 import styles from './tagManagement.module.scss'
 import CreateTag from './CreateTag'
 import TagManagementSolo from "./TagManagementSolo";
+import etv from "../helpers/eventTargetValue";
 export default (props) => {
 
 
 
   const [creatingTag, setCreatingTag] = useState(false)
-    
+  const [filter,setFilter] = useState('')
 
   return(
     <nav className={props.focus?styles.expandleft:styles.retractleft}>
@@ -18,9 +19,12 @@ export default (props) => {
         {creatingTag &&  <CreateTag articles={props.articles} triggerReload={()=>{setCreatingTag(false);props.setNeedReload()}}/>}
         <p className={styles.button} onClick={()=>setCreatingTag(!creatingTag)}>{creatingTag? 'Cancel new Tag' : 'Create new Tag'}</p>
 
+      <input type="text" value={filter} onChange={e=>setFilter(etv(e))} placeholder="Search tag"/>
 
-
-      {props.tags.map((t)=>(
+      {props.tags.filter(t=>{
+        const index = t.name.indexOf(filter)
+        return index !== -1
+      }).map((t)=>(
         <TagManagementSolo {...props} t={t} styles={styles} key={"thistag"+t._id}/>
       ))}
       </div>
