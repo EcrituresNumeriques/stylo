0c30511fcb0e0d9da7d571775ed4751c4c51c35d
lakonis <lakonis@users.noreply.github.com>
2019-09-18 12:51:01 -0400

    update placeholder for default article

--- graphql/models/version.js
+++ graphql/models/version.js
@@ -33,7 +33,11 @@ const versionSchema = new Schema({
   },
   md:{
     type:String,
-    default:'# titre'
+    default:`## section title
+<-- Add your text here, and the document title in the metadatas tool -->
+1. Use only title of level 2 (##) for sections and more (level 1 is used for the article title)
+2. The text is automatically saved. You can version it with [Save as] button
+3. Check the documentation in the menu`
   },
   sommaire:{
     type:String,
