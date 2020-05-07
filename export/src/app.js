94c604c763f3dc93f99cc1a3971affb0e612860d
Guillaume Grossetie <ggrossetie@gmail.com>
2020-05-07 16:07:40 +0200

    Mise à jour et homogénéisation de la configuration pour déploiement en dev

--- export/src/app.js
+++ export/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 const mongoServer = process.env.MONGO_SERVER || localhost
 const mongoServerPort = process.env.MONGO_SERVER_PORT || 27017
 const mongoServerDB = process.env.MONGO_SERVER_DB || 'graphql'
-const listenPort = process.env.NODE_ENV === "Dev" ? 3060:80
+const listenPort = process.env.PORT || 3060
 const redirect = process.env.URL_FRONTEND || 'http://localhost:3000'
 
 
