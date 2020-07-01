ab5ec5ae1f442243384f35a911632055b47ddf25
Guillaume Grossetie <ggrossetie@gmail.com>
2020-07-01 14:34:49 +0200

    resolves #196 amélioration graphique du menu (entête)

--- front/gatsby/src/layouts/Centered.js
+++ front/gatsby/src/layouts/Centered.js
@@ -5,10 +5,10 @@ import Header from '../components/Header'
 import styles from './centered.module.scss'
 
 const Layout = (props) => (
-    <main className={`${styles.grid}`}>
+    <main className={styles.grid}>
         <SEO {...props}/>
         <Header className={styles.header}/>
-        <main className={`${styles.main}`}>
+        <main className={styles.main}>
             {props.children}
         </main>
     </main>
