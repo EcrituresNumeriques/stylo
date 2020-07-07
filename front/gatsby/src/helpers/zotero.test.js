0b49c95a16734e960283617047fad287c27313e8
Guillaume Grossetie <ggrossetie@gmail.com>
2020-07-07 13:06:13 +0200

    Adjust test to update Zotero library

--- front/gatsby/src/helpers/zotero.test.js
+++ front/gatsby/src/helpers/zotero.test.js
@@ -10,8 +10,8 @@ describe('fetchBibliographyFromCollection', () => {
         const zoteroId = '2478772/collections/UGF4W4PZ'
 
         return fetchBibliographyFromCollection(zoteroId).then(bib => {
-            expect(bib[0]).toMatch('@book{noauthor_test1_nodate')
-            expect(bib[0]).not.toMatch('@book{noauthor_test10_nodate')
+            expect(bib[0]).toMatch('@misc{grossetie_hello_nodate')
+            expect(bib[0]).not.toMatch('@book{grossetie_hello_nodate')
             expect(bib[1]).toMatch('@book{noauthor_test10_nodate')
         })
     })
