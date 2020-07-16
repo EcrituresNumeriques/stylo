8029ce28fe43a2f79da60430b423be42fd381922
Guillaume Grossetie <ggrossetie@gmail.com>
2020-07-16 20:02:14 +0200

    Construit la navigation dans une seule condition (pour éviter les effets de bords)

--- front/gatsby/src/components/Header.js
+++ front/gatsby/src/components/Header.js
@@ -1,37 +1,43 @@
 import React from "react"
-import { Link } from "gatsby"
-import { connect } from "react-redux"
+import {Link} from "gatsby"
+import {connect} from "react-redux"
 import env from '../helpers/env'
 
 import styles from './header.module.scss'
 
 // Gestionnaire d'état (quel user est enregistré, enregistré ou non, etc.)
 const mapStateToProps = ({ logedIn, activeUser }) => {
-    return { logedIn, activeUser }
+  return { logedIn, activeUser }
 }
 
 const ConnectedHeader = (props) => {
-    return (
-        <header className={props.className}>
-            <section className={styles.header}>
-                <h1><Link to='/'>_Stylo_</Link></h1>
-                <nav>
-                    {props.logedIn && <>
-                        <Link to='/credentials'>{props.activeUser.displayName}</Link>
-                        <Link to='/books'>Books</Link>
-                        <Link to='/articles'>Articles</Link>
-                        <a href={env.BACKEND_ENDPOINT + '/logout'} className={styles.logoutAction}>Log out</a>
-                    </>}
-                    {!props.logedIn && <Link to="/login">Login</Link>}
-                    {!props.logedIn && <Link to="/register" className={styles.registerAction}>Register</Link>}
-                    <a href='http://stylo-doc.ecrituresnumeriques.ca' className={styles.documentationLink} target='_blank' rel='noopener noreferrer'>Documentation</a>
-                </nav>
-            </section>
-        </header>
-    )
+  const nav = props.logedIn ?
+    <>
+      <Link to='/credentials'>{props.activeUser.displayName}</Link>
+      <Link to='/books'>Books</Link>
+      <Link to='/articles'>Articles</Link>
+      <a href={env.BACKEND_ENDPOINT + '/logout'} className={styles.logoutAction}>Log out</a>
+    </>
+    :
+    <>
+      <Link to="/login">Login</Link>
+      <Link to="/register" className={styles.registerAction}>Register</Link>
+    </>
+
+  return (
+    <header className={props.className}>
+      <section className={styles.header}>
+        <h1><Link to='/'>_Stylo_</Link></h1>
+        <nav>
+          {nav}
+          <a href='http://stylo-doc.ecrituresnumeriques.ca' className={styles.documentationLink} target='_blank' rel='noopener noreferrer'>Documentation</a>
+        </nav>
+      </section>
+    </header>
+  )
 }
 
 const Header = connect(
-    mapStateToProps,
+  mapStateToProps,
 )(ConnectedHeader)
 export default Header
