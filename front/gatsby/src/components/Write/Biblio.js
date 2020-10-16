3de8a4784a2d62c52d5d59144f189fe21dad6409
Guillaume Grossetie <ggrossetie@gmail.com>
2020-10-16 18:51:19 +0200

    Memoize the BibTeX entries (generated from the parser)

--- front/gatsby/src/components/Write/Biblio.js
+++ front/gatsby/src/components/Write/Biblio.js
@@ -1,28 +1,43 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 import Modal from '../Modal'
 import Reference from './Reference'
 import Bibliographe from './bibliographe/Bibliographe'
-import bib2key from './bibliographe/CitationsFilter'
 
 import styles from './biblio.module.scss'
 
 export default (props) => {
-  let entries = bib2key(props.bib)
-  const [expand,setExpand] = useState(true)
+  const [expand, setExpand] = useState(true)
   const [modal, setModal] = useState(false)
 
   return (
-
     <section id={styles.section}>
-      <h1 className={expand ? null : styles.closed} onClick={() => setExpand(!expand)}>{expand ? "-" : "+"} Bibliography</h1>
-      {expand && <>
-        {!props.readOnly && <button onClick={()=>setModal(true)}>Manage Bibliography</button>}
-        {entries.map((entry, index)=>(<Reference key={`ref-${entry.key}-${index}`} entry={entry} />))}
-      </>}
-      {modal && <Modal cancel={()=>setModal(false)}>
-        <Bibliographe bib={props.bib} success={props.handleBib} cancel={()=>setModal(false)} article={props.article}/>
-      </Modal>}
+      <h1
+        className={expand ? null : styles.closed}
+        onClick={() => setExpand(!expand)}
+      >
+        {expand ? '-' : '+'} Bibliography
+      </h1>
+      {expand && (
+        <>
+          {!props.readOnly && (
+            <button onClick={() => setModal(true)}>Manage Bibliography</button>
+          )}
+          {props.bibTeXEntries.map((entry, index) => (
+            <Reference key={`ref-${entry.key}-${index}`} entry={entry} />
+          ))}
+        </>
+      )}
+      {modal && (
+        <Modal cancel={() => setModal(false)}>
+          <Bibliographe
+            bib={props.bib}
+            success={props.handleBib}
+            cancel={() => setModal(false)}
+            article={props.article}
+          />
+        </Modal>
+      )}
     </section>
   )
 }
