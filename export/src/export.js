a51fbb7ff4f9a99f0c2361151aa8363769798da5
Nicolas Sauret <lakonis@users.noreply.github.com>
2020-10-22 14:44:15 +0200

    you know better

--- export/src/export.js
+++ export/src/export.js
@@ -119,7 +119,7 @@ module.exports = {
       }
       const cleanedVersion = version._doc
 
-      exportZIP({ bib: req.params.id.bib, yaml: req.params.id.yaml, md: req.params.id.md, id: req.params.id._id, title: article._doc.title }, res, req)
+      exportZIP({ bib: cleanedVersion.bib, yaml: cleanedVersion.yaml, md: cleanedVersion.md, id: req.params.id, title: article._doc.title }, res, req)
 
     } catch (err) {
       res.status(404).send(err)
