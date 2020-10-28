07f3d046bb3a56e4b1a18e4f3c8faa065b4923df
Guillaume Grossetie <ggrossetie@gmail.com>
2020-10-28 17:14:59 +0100

    Corrige l'URL permettant de comparer les versions

--- front/gatsby/src/components/Write/CompareSelect.js
+++ front/gatsby/src/components/Write/CompareSelect.js
@@ -1,23 +1,47 @@
 import React from 'react'
-import {navigate} from 'gatsby'
+import { navigate } from 'gatsby'
 import styles from './compareSelect.module.scss'
 
-
-export default props => {
-  const handleCompareSelect = e => {
-    if(e.target.value === "false"){
-      navigate(`/article/${props.article._id}/${props.selectedVersion?'version/'+props.selectedVersion:''}`)
+export default (props) => {
+  const handleCompareSelect = (e) => {
+    const articleId = props.article._id
+    const compareVersionId = e.target.value
+    const parts = ['article', articleId]
+    if (props.selectedVersion) {
+      parts.push('version')
+      parts.push(props.selectedVersion)
     }
-    else{
-      navigate(`/article/${props.article._id}/${props.selectedVersion?'version/'+props.selectedVersion:''}/compare/${e.target.value}`)
+    if (compareVersionId !== 'false') {
+      parts.push('compare')
+      parts.push(compareVersionId)
     }
+    // Format:
+    // /article/$articleId
+    // /article/$articleId/compare/$compareVersionId
+    // /article/$articleId/version/$selectedVersionId
+    // /article/$articleId/version/$selectedVersionId/compare/$compareVersionId
+    navigate(`/${parts.join('/')}`)
   }
+
+  const liveVersionLabel = props.live.message || 'No label'
+  const liveVersionTitle = `${liveVersionLabel} v${props.live.version}.${props.live.revision}`
+  const versionTitle = props.readOnly ? liveVersionTitle : 'Editing mode'
+  const title = `Comparing ${versionTitle} with `
   return (
     <p className={styles.compare}>
-        Comparing {props.readOnly?`${props.live.message?props.live.message:'No label'} v${props.live.version}.${props.live.revision}`:'Editing mode'} with <select onChange={(e)=>handleCompareSelect(e)}>
-          <option value={false}>Stop compare</option>
-          {props.versions.map(v=><option value={v._id} key={`versionCompare-${v._id}`} selected={props.compareTo === v._id?true:false}>{v.message?v.message:'No label'} v{v.version}.{v.revision}</option>)}
-        </select>
-      </p>
+      {title}
+      <select onChange={(e) => handleCompareSelect(e)}>
+        <option value={false}>Stop compare</option>
+        {props.versions.map((v) => (
+          <option
+            value={v._id}
+            key={`versionCompare-${v._id}`}
+            selected={props.compareTo === v._id}
+          >
+            {v.message || 'No label'} v{v.version}.{v.revision}
+          </option>
+        ))}
+      </select>
+    </p>
   )
 }
