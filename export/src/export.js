f569186d476a7ae3a67a6e3cd7d645e7a2356c3a
Guillaume Grossetie <ggrossetie@gmail.com>
2020-10-30 15:06:35 +0100

    Utilise archiver pour l'export des tags au format zip

--- export/src/export.js
+++ export/src/export.js
@@ -12,19 +12,7 @@ const canonicalBaseUrl = process.env.EXPORT_CANONICAL_BASE_URL
 
 const exportZIP = ({ bib, yaml, md, id, title }, res, _) => {
   const filename = `${normalize(title)}.zip`
-  const archive = archiver('zip', {
-    zlib: { level: 9 },
-  })
-  archive.on('error', function (err) {
-    res.status(500).send({ error: err.message })
-  })
-  archive.on('end', function () {
-    console.log(`Wrote %d bytes in ${filename}`, archive.pointer())
-  })
-  res.attachment(filename)
-
-  // pipe into the response
-  archive.pipe(res)
+  const archive = createZipArchive(filename, res)
   // add files
   archive.append(Buffer.from(md), { name: `${id}.md` })
   archive.append(Buffer.from(bib), { name: `${id}.bib` })
@@ -145,15 +133,32 @@ const getBookExportContext = async (bookId) => {
   }).populate('versions')
   // sort chapters in ascending alphabetical order
   const chaptersSorted = chapters.sort(sortByTitle)
-  const mds = chaptersSorted.map(
-    (c) => c.versions[c.versions.length - 1].md
-  )
-  const bibs = chaptersSorted.map(
-    (c) => c.versions[c.versions.length - 1].bib
-  )
+  const mds = chaptersSorted.map((c) => c.versions[c.versions.length - 1].md)
+  const bibs = chaptersSorted.map((c) => c.versions[c.versions.length - 1].bib)
   const firstChapter = chaptersSorted[0]
   const { yaml } = firstChapter.versions[firstChapter.versions.length - 1]
-  return { bib: [bib, ...bibs].join('\n'), yaml: yaml, md: mds.join('\n\n'), id, title, }
+  return {
+    bib: [bib, ...bibs].join('\n'),
+    yaml: yaml,
+    md: mds.join('\n\n'),
+    id,
+    title,
+  }
+}
+
+const createZipArchive = (filename, res) => {
+  const archive = archiver('zip', {
+    zlib: { level: 9 },
+  })
+  archive.on('error', function (err) {
+    res.status(500).send({ error: err.message })
+  })
+  archive.on('end', function () {
+    console.log(`Wrote %d bytes in ${filename}`, archive.pointer())
+  })
+  // pipe into the response
+  archive.pipe(res)
+  res.attachment(filename)
 }
 
 module.exports = {
@@ -214,35 +219,23 @@ module.exports = {
     try {
       const tags = req.params.ids.split(',')
       const returnTags = await Tag.find({ _id: { $in: tags } })
-
       const articles = await Article.find({ tags: { $all: tags } }).populate({
         path: 'versions',
         options: { limit: 1, sort: { updatedAt: -1 } },
       })
-      const titles = articles.map((a) => ({
-        title: a._doc.title,
-        md: a._doc.versions[0].md,
-        bib: a._doc.versions[0].bib,
-        yaml: a._doc.versions[0].yaml,
-      }))
-
-      shell.cd('src/data')
-      const dirName = returnTags.map((t) => normalize(t._doc.name)).join('-')
-      shell.exec(`rm -R ${dirName}`)
-      shell.exec(`rm ${dirName}.zip`)
-      shell.exec(`mkdir ${dirName}`)
-      titles.forEach((a) => {
-        const filename = normalize(a.title)
-        shell.echo(a.md).to(`${dirName}/${filename}.md`)
-        shell.echo(a.bib).to(`${dirName}/${filename}.bib`)
-        shell.echo(a.yaml).to(`${dirName}/${filename}.yaml`)
+      const name = returnTags.map((t) => normalize(t._doc.name)).join('-')
+      const filename = `${name}.zip`
+      const archive = createZipArchive(filename, res)
+      // add files
+      articles.forEach((article) => {
+        const filename = normalize(article._doc.title)
+        const { md, bib, yaml } = article._doc.versions[0]
+        archive.append(Buffer.from(md), { name: `${filename}.md` })
+        archive.append(Buffer.from(bib), { name: `${filename}.bib` })
+        archive.append(Buffer.from(yaml), { name: `${filename}.yaml` })
       })
-      const ls = shell.ls(dirName)
-      shell.exec(`zip ${dirName}.zip ${dirName}/*`)
-      shell.exec(`rm -R ${dirName}`)
-      shell.cd('../../')
-      res.set('Content-Disposition', `attachment; filename="${dirName}.zip"`)
-      return res.download(`${process.env.PWD}/src/data/${dirName}.zip`)
+      // zip!
+      archive.finalize()
     } catch (err) {
       errorHandler(err, res)
     }
