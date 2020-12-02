8f45a93b0617092e5e994460e234efc86a0d44c9
Guillaume Grossetie <ggrossetie@gmail.com>
2020-12-02 11:34:41 +0100

    Ajout du code technique

--- front/gatsby/src/pages/error.js
+++ front/gatsby/src/pages/error.js
@@ -4,22 +4,29 @@ import App from '../layouts/App'
 import styles from '../components/Write/write.module.scss'
 
 export default () => {
-  // SEE https://www.oauth.com/oauth2-servers/authorization/the-authorization-response/
-  const params = new URLSearchParams(document.location.search.substring(1))
-  const error = params.get('error')
-  const errorDescription = params.get('error_description')
+  let error = ""
+  let errorDescription = ""
+
+  if (typeof window !== 'undefined') {
+    // see https://www.oauth.com/oauth2-servers/authorization/the-authorization-response/
+    const params = new URLSearchParams(document.location.search)
+    error = params.get('error')
+    errorDescription = params.get('error_description')
+  }
 
   return (
-  <App layout="fullPage">
-    <section className={styles.container}>
-      <article className={styles.error}>
-        <h2>Authentication Error</h2>
+    <App layout="fullPage">
+      <section className={styles.container}>
+        <article className={styles.error}>
+          <h2>Authentication Error</h2>
 
-        <p>The authentication process could not go through{!errorDescription && `, and it's not clear why`}.</p>
+          <p>The authentication process could not go through{!errorDescription && `, and it's not clear why`}.</p>
 
-        {errorDescription && <p>{ errorDescription }</p>}
-      </article>
-    </section>
-  </App>
-)
+          {errorDescription && <p>{ errorDescription }</p>}
+
+          {error && <code>{error}</code>}
+        </article>
+      </section>
+    </App>
+  )
 }
