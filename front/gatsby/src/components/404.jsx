6292d99fbdbf8a05379d476372ad01893ac7c466
Guillaume Grossetie <ggrossetie@gmail.com>
2021-01-13 17:25:15 +0100

    Restaure la page 404 et corrige le routing

---
+++ front/gatsby/src/components/404.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+import App from '../layouts/App'
+
+import styles from '../components/Write/write.module.scss'
+
+export default () => {
+  const location = useLocation()
+  return (
+    <App layout="fullPage">
+      <section className={styles.container}>
+        <article className={styles.error}>
+          <h2>Error 404</h2>
+
+          <p>Page not found at <code>{location.pathname}</code>.</p>
+        </article>
+      </section>
+    </App>
+  )
+}
