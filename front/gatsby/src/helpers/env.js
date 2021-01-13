91951ed736ec655f5e313196357b7048d8b27e0b
Guillaume Grossetie <ggrossetie@gmail.com>
2021-01-13 09:18:29 +0100

    Migre les variables d'environnement

--- front/gatsby/src/helpers/env.js
+++ front/gatsby/src/helpers/env.js
@@ -3,14 +3,20 @@ const backendEndpoint =
 const graphqlEndpoint =
   import.meta.env.SNOWPACK_PUBLIC_GRAPHQL_ENDPOINT ||
   'http://localhost:3030/graphql'
+const exportEndpoint =
+  import.meta.env.SNOWPACK_PUBLIC_EXPORT_ENDPOINT || 'http://localhost:3060'
+const processEndpoint =
+  import.meta.env.SNOWPACK_PUBLIC_PROCESS_ENDPOINT || 'http://localhost:9090'
+const humanIdRegisterEndpoint =
+  import.meta.env.SNOWPACK_PUBLIC_HUMAN_ID_REGISTER_ENDPOINT ||
+  'https://auth-test.huma-num.fr/register?service=http://localhost:3030/authorization-code/callback'
 
 const env = {
   BACKEND_ENDPOINT: backendEndpoint,
   GRAPHQL_ENDPOINT: graphqlEndpoint,
-  EXPORT_ENDPOINT: 'http://localhost:3060',
-  PROCESS_ENDPOINT: 'http://localhost:9090',
-  HUMAN_ID_REGISTER_ENDPOINT:
-    'https://auth-test.huma-num.fr/register?service=http://localhost:3030/authorization-code/callback',
+  EXPORT_ENDPOINT: exportEndpoint,
+  PROCESS_ENDPOINT: processEndpoint,
+  HUMAN_ID_REGISTER_ENDPOINT: humanIdRegisterEndpoint,
 }
 
 export default env
