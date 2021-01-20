ac99bb376db87ac5c33a999fd82aa3390393e927
Thomas Parisot <oncletom@users.noreply.github.com>
2021-01-20 15:42:12 +0100

    fixup! Enable Snowpack React fast refresh

--- front/gatsby/snowpack.config.cjs
+++ front/gatsby/snowpack.config.cjs
@@ -3,7 +3,10 @@ module.exports = {
     public: { url: '/', static: true },
     src: { url: '/dist' },
   },
-  plugins: ['@snowpack/plugin-sass'],
+  plugins: [
+    '@snowpack/plugin-sass',
+    '@snowpack/plugin-react-refresh'
+  ],
   devOptions: {
     port: 3000,
   },
