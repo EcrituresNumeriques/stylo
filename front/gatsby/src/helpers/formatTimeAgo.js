ae7aa41309cf6d4ce66907ceaebb0e211a7b21ef
Guillaume Grossetie <ggrossetie@gmail.com>
2021-01-20 18:33:15 +0100

    Réutilisation de composants entre la page books et articles

---
+++ front/gatsby/src/helpers/formatTimeAgo.js
@@ -0,0 +1,25 @@
+const formatter = new Intl.RelativeTimeFormat(undefined, {
+  numeric: 'auto'
+})
+
+const DIVISIONS = [
+  { amount: 60, name: 'seconds' },
+  { amount: 60, name: 'minutes' },
+  { amount: 24, name: 'hours' },
+  { amount: 7, name: 'days' },
+  { amount: 4.34524, name: 'weeks' },
+  { amount: 12, name: 'months' },
+  { amount: Number.POSITIVE_INFINITY, name: 'years' }
+]
+
+export default (date) => {
+  let duration = (date - new Date()) / 1000
+
+  for (let i = 0; i <= DIVISIONS.length; i++) {
+    const division = DIVISIONS[i]
+    if (Math.abs(duration) < division.amount) {
+      return formatter.format(Math.round(duration), division.name)
+    }
+    duration /= division.amount
+  }
+}
