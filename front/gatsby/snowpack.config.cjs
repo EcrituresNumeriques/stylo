d57927a5420e4beb5263f6ef56b8d30129275572
Thomas Parisot <oncletom@users.noreply.github.com>
2021-01-22 12:00:37 +0100

    Use `snowpack@3` code splitting, and dynamically load heavy components

--- front/gatsby/snowpack.config.cjs
+++ front/gatsby/snowpack.config.cjs
@@ -1,7 +1,17 @@
+const production = process.env.NODE_ENV === 'production'
+const sourcemaps = !production || process.env.ENABLE_SOURCEMAPS
+
 module.exports = {
   mount: {
     public: { url: '/', static: true },
     src: { url: '/dist' },
+  },
+  "optimize": {
+    "bundle": true,
+    "minify": production,
+    "sourcemaps": sourcemaps,
+    "splitting": true,
+    "treeshake": true
   },
   plugins: [
     '@snowpack/plugin-sass',
