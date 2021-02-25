11384cdc99bad42901e4f72bfad23e99d81817fb
Thomas Parisot <oncletom@users.noreply.github.com>
2021-02-25 11:44:20 +0100

    Convert date metadata during export

--- export/src/helpers/metadata.js
+++ export/src/helpers/metadata.js
@@ -1,15 +1,41 @@
-const prepare = (yaml, id) => {
-  // remove lines that contain "bibliography"
-  yaml = yaml.replace(/\n.*bibliography.*/gm, '')
-  // remove the end YAML delimiter (i.e. "---")
-  yaml = yaml.replace(/\n---\n*$/, '')
-  // add bibliography link
-  yaml = yaml.concat(`
-bibliography: ${id}.bib
----`)
-  return yaml
+const YAML = require('js-yaml')
+
+const canonicalBaseUrl = process.env.EXPORT_CANONICAL_BASE_URL
+const prepare = (yaml, {id, originalUrl, replaceBibliography = false}) => {
+  // the YAML contains a single document enclosed in "---" to satisfy pandoc
+    // thereby, we need to use "load all":
+    const docs = YAML.loadAll(yaml, 'utf8')
+    // contains only a single document
+    const doc = docs[0]
+
+    if (canonicalBaseUrl && originalUrl) {
+      // add link-canonical to the first (and only) document
+      doc['link-canonical'] = canonicalBaseUrl + originalUrl
+    }
+
+    // add a default title if missing or empty
+    const title = doc.title
+    if (!title || title.trim().length === 0) {
+      doc.title = 'untitled'
+    }
+
+    if (replaceBibliography) {
+      doc.bibliography = `${id}.bib`
+    }
+
+    if (doc.date) {
+      const dateString = doc.date.replace(/\//g, '-')
+      const [year, month, day] = dateString.split('-')
+      doc.date = `${year}/${month}/${day}`
+      doc.day = day
+      doc.month = month
+      doc.year = year
+    }
+
+  // dump the result enclosed in "---"
+  return '---\n' + YAML.dump(doc, { sortKeys: true }) + '---'
 }
 
 module.exports = {
-  prepare: prepare
+  prepare
 }
--- export/src/helpers/metadata.test.js
+++ export/src/helpers/metadata.test.js
@@ -10,14 +10,42 @@ authors:
     surname: Vitali-Rosati
     orcid: 0000-0001-6424-3229
 ---
-`, 'abcd1234')).toBe(`---
-title: Stylo
-subtitle: A user friendly text editor for humanities scholars.
+`, {id: 'abcd1234', replaceBibliography: true})).toBe(`---
 authors:
   - forname: Marcello
-    surname: Vitali-Rosati
     orcid: 0000-0001-6424-3229
+    surname: Vitali-Rosati
 bibliography: abcd1234.bib
+subtitle: A user friendly text editor for humanities scholars.
+title: Stylo
+---`
+  )
+})
+
+test('year/month/day are derived from date', () => {
+  expect(prepare(`---
+title: Stylo
+date: '2021-02-25'
+---
+`, {id: 'abcd1234'})).toBe(`---
+date: 2021/02/25
+day: '25'
+month: '02'
+title: Stylo
+year: '2021'
+---`
+  )
+
+  expect(prepare(`---
+title: Stylo
+date: '2021/02/25'
+---
+`, {id: 'abcd1234'})).toBe(`---
+date: 2021/02/25
+day: '25'
+month: '02'
+title: Stylo
+year: '2021'
 ---`
   )
 })
