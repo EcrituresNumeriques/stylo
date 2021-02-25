35e00a405cd61e884d546fa24538876ae6753613
Thomas Parisot <oncletom@users.noreply.github.com>
2021-02-25 16:18:15 +0100

    Rename `YAML.safeLoadAll()` into `YAML.loadAll()`

--- front/gatsby/src/components/Write/yamleditor/YamlEditor.jsx
+++ front/gatsby/src/components/Write/yamleditor/YamlEditor.jsx
@@ -3,7 +3,7 @@ import Form from '../../Form'
 import YAML from 'js-yaml'
 
 export default ({ yaml, basicMode, onChange }) => {
-  const [parsed] = YAML.safeLoadAll(yaml)
+  const [parsed] = YAML.loadAll(yaml)
 
   // we convert YYYY/MM/DD dates into ISO YYYY-MM-DD
   if (parsed.date) {
