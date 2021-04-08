af3e651d14362eb9f7790299921b471923507b67
Thomas Parisot <oncletom@users.noreply.github.com>
2021-04-08 07:00:33 +0200

    Remove unusued provider.jsx file

--- front/gatsby/src/components/provider.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import createStore from '../createReduxStore.js'
-
-export const store = createStore()
-
-export default ({ element }) => {
-  return <Provider store={store}>{element}</Provider>
-}
