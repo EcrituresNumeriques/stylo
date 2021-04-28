fece88b7a777108a558d08f6d6054c579ecec0de
Thomas Parisot <oncletom@users.noreply.github.com>
2021-04-28 17:17:08 +0200

    Modularize Redux Store

--- front/gatsby/src/createReduxStore.js
+++ front/gatsby/src/createReduxStore.js
@@ -183,4 +183,4 @@ function updateArticleWriters (state, { articleWriters }) {
   return { ...state, articleWriters }
 }
 
-export default () => createStore(reducer, initialState, , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export default () => createStore(reducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
