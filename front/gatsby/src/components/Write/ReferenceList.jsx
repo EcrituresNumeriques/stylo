a482f662ca592b05bcf986f88ae317f1a76bd99e
Guillaume Grossetie <ggrossetie@gmail.com>
2021-05-05 14:49:27 +0200

    Filtre les références bibliographiques et affiche les 10 premiers résultats

--- front/gatsby/src/components/Write/ReferenceList.jsx
+++ front/gatsby/src/components/Write/ReferenceList.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
+import { Search } from 'react-feather'
 
 import Reference from './Reference'
+import styles from './ReferenceList.module.scss'
+import Field from '../Field'
 
-function ReferenceList ({ articleBibTeXEntries }) {
+function ReferenceList({ articleBibTeXEntries }) {
+  const [filter, setFilter] = useState('')
+
+  const bibTeXFound = articleBibTeXEntries
+    .filter((entry) => entry.key.toLowerCase().indexOf(filter.toLowerCase()) > -1)
+
   return (
     <>
-      {articleBibTeXEntries
+      <Field className={styles.searchField} type="text" icon={Search} value={filter} placeholder="Search" onChange={(e) => setFilter(e.target.value)} />
+      <span className={styles.resultFoundCount}>{bibTeXFound.length} found</span>
+      {bibTeXFound
+        .slice(0, 10)
         .map((entry, index) => (
-          <Reference key={`ref-${entry.key}-${index}`} entry={entry}/>
+          <Reference key={`ref-${entry.key}-${index}`} entry={entry} />
         ))
       }
+      {bibTeXFound.length > 10 && <span className={styles.more}>&hellip;</span>}
     </>
   )
 }
