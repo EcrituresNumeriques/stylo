95b517f2c35acc70270e666a4a0dc9bfa47e518e
Guillaume Grossetie <ggrossetie@gmail.com>
2021-06-02 17:44:56 +0200

    Configure SameSite=None pour le cross-domain

--- graphql/app.js
+++ graphql/app.js
@@ -58,10 +58,9 @@ const secureCookie = process.env.HTTPS === 'true'
 // When using 'strict' value, cookies will not be sent along with requests initiated by third-party websites.
 // Reference: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
 const allowedOrigins = (origin ?? '').split(' ').filter(v => v).map(o => new RegExp('^' + o))
-const sameSiteCookies = process.env.NODE_ENV === 'production' ? 'lax' : 'none'
-if (sameSiteCookies === 'none') {
-  console.warn('Cookies are configured with `sameSite: none`.')
-}
+// SameSite should be None on cross-site response.
+// Please note that "SameSite=None" must also specify the Secure attribute (they require a secure context/HTTPS).
+const sameSiteCookies = allowedOrigins.length > 1 && secureCookie ? 'none' : 'lax'
 
 const corsOptions = {
   optionsSuccessStatus: 200,
