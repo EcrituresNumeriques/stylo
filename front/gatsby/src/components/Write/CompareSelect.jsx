3bf3160ccd9317c853566db477de9d3ad614e86b
Guillaume Grossetie <g.grossetie@gmail.com>
2021-08-24 12:14:24 +0200

    Importe useCallback

--- front/gatsby/src/components/Write/CompareSelect.jsx
+++ front/gatsby/src/components/Write/CompareSelect.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { useHistory } from 'react-router-dom'
 
 import styles from './compareSelect.module.scss'
