cecd54123df7766a220b895f164f3ecc34c7b763
Thomas Parisot <oncletom@users.noreply.github.com>
2021-11-18 00:23:51 +0100

    Expose base url if available, and split codemirror in its own vendor file

--- front/gatsby/vite.config.js
+++ front/gatsby/vite.config.js
@@ -4,8 +4,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: env.DEPLOY_PRIME_URL ?? '/',
   build: {
     outDir: 'build',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          codemirror: ['react-codemirror2', 'codemirror']
+        }
+      }
+    }
   },
   plugins: [
     react(),
@@ -15,7 +23,5 @@ export default defineConfig({
       NODE_ENV: env.NODE_ENV
     },
     'global.PREVENT_CODEMIRROR_RENDER': false,
-  },
-  optimizeDeps: {
   }
 })
