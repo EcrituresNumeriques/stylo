462a6b2dfa4fa7fcf5bc8b728e29cf03ee96b864
Thomas Parisot <oncletom@users.noreply.github.com>
2021-11-17 15:01:57 +0100

    Remove `unlinkVersion` GraphQL mutation (unused)

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -185,9 +185,6 @@ type RootMutation {
   "Save a new version for article [need to be authentificated as specified user]"
   saveVersion(version:VersionInput!,user:ID!):Version!
 
-  "Unlink a version from article [need to be authentificated as specified user]"
-  unlinkVersion(version:ID!,user:ID!):Version!
-
   "Create tag [need to be authentificated as specified user]"
   createTag(name:String!,description:String,user:ID!):Tag!
 
