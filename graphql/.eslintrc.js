38f3c7c59d2ae8241b166ebdbcf43e5a8e3320ad
Thomas Parisot <oncletom@users.noreply.github.com>
2021-11-17 11:31:41 +0100

    Add eslint to catch unused variables while developping
    refs #439

---
+++ graphql/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+    ],
+    "parserOptions": {
+        "ecmaVersion": 13
+    },
+    "plugins": [
+    ],
+    "rules": {
+      "no-useless-catch": 0
+    }
+};
