ac35fff62fdee792aa03369baba7d46c42e9fcf9
Guillaume Grossetie <ggrossetie@gmail.com>
2021-11-17 19:08:17 +0100

    Mise à jour de la date de dernier enregistrement

--- front/gatsby/src/helpers/formatTimeAgo.js
+++ front/gatsby/src/helpers/formatTimeAgo.js
@@ -15,8 +15,9 @@ const DIVISIONS = [
 export default (date) => {
   let duration = (date - new Date()) / 1000
 
-  if (duration < 60) {
-    return 'less than 1 minute ago'
+  console.log('duration', Math.abs(duration))
+  if (Math.abs(duration) < 60) {
+    return 'a few seconds ago'
   }
 
   for (let i = 0; i <= DIVISIONS.length; i++) {
