98a04d0c24670dc297f54f6cd101ca500b9d7e05
Thomas Parisot <oncletom@users.noreply.github.com>
2021-11-17 15:01:06 +0100

    Add an `updateArticle` GraphQL mutation, and remove autosave from `saveVersion`

--- graphql/resolvers/versionResolver.js
+++ graphql/resolvers/versionResolver.js
@@ -10,71 +10,47 @@ const populateArgs = require('../helpers/populateArgs')
 
 module.exports = {
   saveVersion: async (args,{req}) => {
+    args = populateArgs(args,req)
+    isUser(args,req)
 
-    //filter bad requests
-    try{
-      args = populateArgs(args,req)
-      isUser(args,req)
+    //fetch user
+    const thisUser = await User.findOne({_id:args.user})
+    if(!thisUser){throw new Error('This user does not exist')}
 
+    //fetch article
+    const articleToSaveInto = await Article.findOne({_id:args.version.article}).populate('versions owners');
+    if(!articleToSaveInto){throw new Error('Wrong article ID')}
 
-      //fetch user
-      const thisUser = await User.findOne({_id:args.user})
-      if(!thisUser){throw new Error('This user does not exist')}
-
-      //fetch article
-      const articleToSaveInto = await Article.findOne({_id:args.version.article}).populate('versions owners');
-      if(!articleToSaveInto){throw new Error('Wrong article ID')}
-
-      if(!articleToSaveInto.owners.map(u => u.id).includes(thisUser.id)){
-        throw new Error('User has no right to push new version')
-      }
-
-      const lastVersion = articleToSaveInto.versions[articleToSaveInto.versions.length-1]
-      let values = {...lastVersion._doc}
-      values = {...values,message:args.version.message,owner:thisUser.id}
-      if(args.version.md){
-        values = {...values, md:args.version.md}
-        values = {...values, sommaire:args.version.md.split('\n').filter((line) => (line.match(/^#+\ /))).join('\n')}
-      }
-      if(args.version.yaml){
-        values = {...values, yaml:args.version.yaml}
-      }
-      if(args.version.bib){
-        values = {...values, bib:args.version.bib}
-      }
-
-      delete values.createdAt
-      delete values.updatedAt
-
-      if(args.version.auto && lastVersion.owner == thisUser.id){
-          //Updating last autosave
-          await articleToSaveInto.update({
-            workingVersion: { $set:{...values} }
-          })
-
-          // we save data we already have on the client-side
-          return 201
-      }
-      else{
-        //full save, add new record
-        delete values._id
-
-        //Add major or not + set autosave status
-        if(args.version.major){values.version++;values.revision=0}
-        else{values.revision++}
-
-        const returnedVersion = await Version.create({...values})
-        articleToSaveInto.versions.push(returnedVersion)
-        await articleToSaveInto.save()
-        return populateVersion(returnedVersion)
-      }
-
-
-    }
-    catch(err){
-      throw err
+    if(!articleToSaveInto.owners.map(u => u.id).includes(thisUser.id)){
+      throw new Error('User has no right to push new version')
     }
 
+    const lastVersion = articleToSaveInto.versions[articleToSaveInto.versions.length-1]
+    let values = {...lastVersion._doc}
+    values = {...values,message:args.version.message,owner:thisUser.id}
+    if(args.version.md){
+      values = {...values, md:args.version.md}
+      values = {...values, sommaire:args.version.md.split('\n').filter((line) => (line.match(/^#+\ /))).join('\n')}
+    }
+    if(args.version.yaml){
+      values = {...values, yaml:args.version.yaml}
+    }
+    if(args.version.bib){
+      values = {...values, bib:args.version.bib}
+    }
+
+    delete values.createdAt
+    delete values.updatedAt
+    delete values._id
+
+    //Add major or not
+    if(args.version.major){values.version++;values.revision=0}
+    else{values.revision++}
+
+    const returnedVersion = await Version.create({...values})
+    articleToSaveInto.versions.push(returnedVersion)
+    await articleToSaveInto.save()
+    return populateVersion(returnedVersion)
   },
   unlinkVersion: async (args,{req})=>{
     try{
