0fdd5301ff2e71dda45117a15678aab004429339
Thomas Parisot <oncletom@users.noreply.github.com>
2021-11-18 10:04:34 +0100

    Use CommonJS config for Babel

--- front/gatsby/babel.config.cjs
+++ front/gatsby/babel.config.cjs
@@ -1,10 +1,9 @@
-import { env } from 'process'
-
-export default {
+/* eslint-disable no-undef */
+module.exports = {
   "presets": [
     ['@babel/preset-react', {
       runtime: 'automatic',
-      development: env.NODE_ENV === 'development',
+      development: process.env.NODE_ENV === 'development',
       importSource: '@welldone-software/why-did-you-render',
     }],
     [
