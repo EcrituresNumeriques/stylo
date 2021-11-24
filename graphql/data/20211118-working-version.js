8a01d7d159ba6e92028c2535551537f88a320ac9
Guillaume Grossetie <ggrossetie@gmail.com>
2021-11-24 15:20:43 +0100

    Fix script syntax and format with eslint

--- graphql/data/20211118-working-version.js
+++ graphql/data/20211118-working-version.js
@@ -1,31 +1,36 @@
+/* global print, Mongo */
 const conn = Mongo()
 const db = conn.getDB('stylo-prod')
 
 // @see https://mongoosejs.com/docs/transactions.html
-db.transaction((session) => {
-	  const cursor = db.articles.find({})
-	  while (cursor.hasNext()) {
-	    const article = cursor.next()
-	    const title = article.title
-	    const latestVersionId = article.versions.slice(-1)[0]
-	    //print(`Processing article ${article._id}...`)
-	    if (typeof article.workingVersion === 'undefined') {
-	      if (latestVersionId) {
-	        const latestVersion = db.versions.findOne({_id: latestVersionId})
-	        db.articles.updateOne(
-	          { _id: article._id },
-	          { $set: { 'workingVersion': { bib: latestVersion.bib, md: latestVersion.md, yaml: latestVersion.yaml } } },
-	          { upsert: false }
-	        )
-	        // if the latest version is "autosave", remove!
-	        db.versions.remove(
-	          { _id: latestVersionId, autosave: true }
-	        )
-	      } else {
-	        print(`Article ${article._id} has no version and no workingVersion!`)
-	      }
-	    } else {
-	      // article has already a workingVersion, skipping!
-	    }
-	  }
-}
+db.transaction(() => {
+  const cursor = db.articles.find({})
+  while (cursor.hasNext()) {
+    const article = cursor.next()
+    const latestVersionId = article.versions.slice(-1)[0]
+    if (typeof article.workingVersion === 'undefined') {
+      if (latestVersionId) {
+        const latestVersion = db.versions.findOne({ _id: latestVersionId })
+        db.articles.updateOne(
+          { _id: article._id },
+          {
+            $set: {
+              workingVersion: {
+                bib: latestVersion.bib,
+                md: latestVersion.md,
+                yaml: latestVersion.yaml,
+              },
+            },
+          },
+          { upsert: false }
+        )
+        // if the latest version is "autosave", remove!
+        db.versions.remove({ _id: latestVersionId, autosave: true })
+      } else {
+        print(`Article ${article._id} has no version and no workingVersion!`)
+      }
+    } else {
+      // article has already a workingVersion, skipping!
+    }
+  }
+})
