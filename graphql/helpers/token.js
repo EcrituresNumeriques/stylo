389da72abf9c11a9235824da13d4f42bd05ec1fa
Thomas Parisot <oncletom@users.noreply.github.com>
2021-12-03 07:42:02 +0100

    `populateUserFromJWT()` does not read from cookie anymore

--- graphql/helpers/token.js
+++ graphql/helpers/token.js
@@ -18,9 +18,7 @@ module.exports.createJWTToken = async function createJWTToken ({ email, jwtSecre
 
 module.exports.populateUserFromJWT = function populateUserFromJWT ({ jwtSecret }) {
   return function populateUserFromJWTMiddleware(req, res, next) {
-    const jwtToken = req.headers.authorization
-      ? req.headers.authorization.replace(/^Bearer /, '')
-      : req.cookies['graphQL-jwt']
+    const jwtToken = req.headers.authorization?.replace(/^Bearer /, '')
 
     if (typeof jwtToken === 'string') {
       try {
