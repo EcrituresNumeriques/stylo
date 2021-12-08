1a4dd752ded42784135f3790658dd73484db767d
Thomas Parisot <oncletom@users.noreply.github.com>
2021-12-08 06:53:55 +0100

    Ensure application boots with default values if `config.json` does not exist
    Which is the case after a fresh `git clone` and `npm install` of the project

--- front/gatsby/src/helpers/applicationConfig.js
+++ front/gatsby/src/helpers/applicationConfig.js
@@ -1,5 +1,12 @@
-export function getApplicationConfig() {
-  return fetch(`/config.json`, {
-    method: 'GET',
-  }).then((res) => res.json())
+export function getApplicationConfig(defaultValues) {
+  return fetch(`/config.json`).then(response => {
+    // this is the path taken when config is server-side generated
+    if (response.ok) {
+      return response.json()
+    }
+
+    // otherwise, we use environment variable based store values
+    console.warn('%c[Stylo]', 'font-weight: bold', 'Using default `applicationConfig` store values.')
+    return defaultValues
+  })
 }
