bcad83136933cfc18ced104b62b15163222b4049
Thomas Parisot <oncletom@users.noreply.github.com>
2021-12-14 08:08:09 +0100

    Use `gql` template string to build schema

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,167 +1,183 @@
-const { buildSchema } = require('graphql');
+const gql = require('graphql-tag')
+const { buildASTSchema } = require('graphql')
 
+module.exports = buildASTSchema(gql`
+  type User {
+    _id: ID!
+    displayName: String!
+    email: String!
+    firstName: String
+    lastName: String
+    institution: String
+    tags(limit: Int, page: Int): [Tag!]!
+    acquintances(limit: Int, page: Int): [User!]!
+    articles(limit: Int, page: Int): [Article!]!
+    admin: Boolean
+    yaml: String
+    zoteroToken: String
+    createdAt: String
+    updatedAt: String
+  }
 
-module.exports = buildSchema(`
-type User {
-  _id: ID!
-  displayName: String!
-  email: String!
-  firstName:String
-  lastName:String
-  institution:String
-  tags(limit:Int,page:Int):[Tag!]!
-  acquintances(limit:Int,page:Int):[User!]!
-  articles(limit:Int,page:Int):[Article!]!
-  admin:Boolean
-  yaml:String
-  zoteroToken:String
-  createdAt:String
-  updatedAt:String
-}
+  type Tag {
+    _id: ID!
+    name: String!
+    description: String
+    color: String
+    articles: [Article!]!
+    createdAt: String
+    updatedAt: String
+  }
 
-type Tag{
-  _id: ID!
-  name:String!
-  description:String
-  color:String
-  articles:[Article!]!
-  createdAt:String
-  updatedAt:String
-}
+  type WorkingVersion {
+    md: String
+    yaml: String
+    bib: String
+  }
 
-type WorkingVersion {
-  md:String
-  yaml:String
-  bib:String
-}
+  type Version {
+    _id: ID!
+    version: Int
+    revision: Int
+    md: String
+    sommaire: String
+    yaml: String
+    bib: String
+    message: String
+    article: Article
+    owner: User
+    createdAt: String
+    updatedAt: String
+  }
 
-type Version{
-  _id: ID!
-  version:Int
-  revision:Int
-  md:String
-  sommaire:String
-  yaml:String
-  bib:String
-  message:String
-  article:Article
-  owner:User
-  createdAt:String
-  updatedAt:String
-}
+  type Article {
+    _id: ID!
+    title: String
+    zoteroLink: String
+    owners(limit: Int, page: Int): [User!]!
+    workingVersion: WorkingVersion
+    versions(limit: Int, page: Int): [Version!]!
+    tags(limit: Int, page: Int): [Tag!]!
+    createdAt: String
+    updatedAt: String
+  }
 
-type Article {
-  _id: ID!
-  title: String
-  zoteroLink: String
-  owners(limit:Int,page:Int): [User!]!
-  workingVersion: WorkingVersion
-  versions(limit:Int,page:Int): [Version!]!
-  tags(limit:Int,page:Int):[Tag!]!
-  createdAt:String
-  updatedAt:String
-}
+  input UserInput {
+    email: String!
+    username: String
+    password: String!
+    displayName: String
+    firstName: String
+    lastName: String
+    institution: String
+  }
 
-input UserInput {
-  email: String!
-  username: String
-  password: String!
-  displayName: String
-  firstName: String
-  lastName: String
-  institution: String
-}
+  input VersionInput {
+    article: ID!
+    major: Boolean
+    message: String
+  }
 
-input VersionInput {
-  article:ID!
-  major:Boolean
-  message: String
-}
+  type RootQuery {
+    "Fetch all articles [Reserved for admins]"
+    articles: [Article!]!
 
-type RootQuery {
-  "Fetch all articles [Reserved for admins]"
-  articles:[Article!]!
+    "Fetch all users [Reserved for admins]"
+    users: [User!]!
 
-  "Fetch all users [Reserved for admins]"
-  users:[User!]!
+    "Fetch authenticated user info"
+    user(user: ID): User!
 
-  "Fetch authenticated user info"
-  user(user:ID):User!
+    "Fetch article info [need to have acces to this article]"
+    article(article: ID!): Article!
 
-  "Fetch bearer token based on your cookie graphQL-jwt [Need to be loged in previously and send the appropriate cookie]"
-  refreshToken(expiration:String):AuthToken
+    "Fetch version info"
+    version(version: ID!): Version!
+  }
 
-  "Fetch article info [need to have acces to this article]"
-  article(article:ID!):Article!
+  type RootMutation {
+    "Create user + password + default article"
+    createUser(user: UserInput!): User!
 
-  "Fetch version info"
-  version(version:ID!):Version!
-}
+    "Add an email to your acquintances [need to be authentificated as user]"
+    addAcquintance(email: String!, user: ID!): User!
 
-type RootMutation {
+    "Change password"
+    changePassword(old: String!, new: String!, user: ID!): User!
 
-  "Create user + password + default article"
-  createUser(user:UserInput!):User!
+    "Change user information"
+    updateUser(
+      user: ID!
+      displayName: String
+      firstName: String
+      lastName: String
+      institution: String
+      zoteroToken: String
+      yaml: String
+    ): User!
 
-  "Add an email to your acquintances [need to be authentificated as user]"
-  addAcquintance(email:String!,user:ID!):User!
+    "Give access to a user using a password's email"
+    addCredential(email: String!, user: ID!): User!
+    # If need to create new user: createUser + addCredential
 
-  "Change password"
-  changePassword(old:String!,new:String!,user:ID!):User!
+    "Remove access to a user using a password's email (can't be the main email)"
+    removeCredential(email: String!, user: ID!): User!
 
-  "Change user information"
-  updateUser(user:ID!,displayName:String,firstName:String,lastName:String,institution:String,zoteroToken:String,yaml:String):User!
+    "Create article for specified user [need to be authentificated as specified user]"
+    createArticle(title: String!, user: ID!): Article!
 
-  "Give access to a user using a password's email"
-  addCredential(email:String!,user:ID!):User!
-  # If need to create new user: createUser + addCredential
+    "Update article bibliography, content or metadata [need to be authentificated as specified user]"
+    updateWorkingVersion(
+      user: ID!
+      article: ID!
+      bib: String
+      md: String
+      yaml: String
+    ): Article!
 
-  "Remove access to a user using a password's email (can't be the main email)"
-  removeCredential(email:String!,user:ID!):User!
+    "Save a new version for article [need to be authentificated as specified user]"
+    saveVersion(version: VersionInput!, user: ID!): Version!
 
-  "Create article for specified user [need to be authentificated as specified user]"
-  createArticle(title:String!,user:ID!):Article!
+    "Create tag [need to be authentificated as specified user]"
+    createTag(name: String!, description: String, user: ID!): Tag!
 
-  "Update article bibliography, content or metadata [need to be authentificated as specified user]"
-  updateWorkingVersion(user:ID!,article:ID!,bib:String,md:String,yaml:String):Article!
+    "update name and description of a tag [need to be authentificated as specified user]"
+    updateTag(
+      name: String
+      description: String
+      color: String
+      tag: ID!
+      user: ID!
+    ): Tag!
 
-  "Save a new version for article [need to be authentificated as specified user]"
-  saveVersion(version:VersionInput!,user:ID!):Version!
+    "Delete tag and all articles carrying it"
+    deleteTag(tag: ID!, user: ID!): User!
 
-  "Create tag [need to be authentificated as specified user]"
-  createTag(name:String!,description:String,user:ID!):Tag!
+    "Add article to a specified tag [Need to be authenficated as owner of the tag]"
+    addToTag(article: ID!, tag: ID!, user: ID!): Article!
 
-  "update name and description of a tag [need to be authentificated as specified user]"
-  updateTag(name:String,description:String,color:String,tag:ID!,user:ID!):Tag!
+    "Remove article from a specified tag [Need to be authenficated as owner of the tag]"
+    removeFromTag(article: ID!, tag: ID!, user: ID!): Article!
 
-  "Delete tag and all articles carrying it"
-  deleteTag(tag:ID!,user:ID!):User!
+    "add a user to an article"
+    shareArticle(article: ID!, to: ID!, user: ID!): Article!
 
-  "Add article to a specified tag [Need to be authenficated as owner of the tag]"
-  addToTag(article:ID!,tag:ID!,user:ID!):Article!
+    "Send last version, or specified version, to an user"
+    sendArticle(article: ID!, version: ID, to: ID!, user: ID!): Article! # this is a copy of last version OR specified version
+    # fork is sendArticle to yourself
 
-  "Remove article from a specified tag [Need to be authenficated as owner of the tag]"
-  removeFromTag(article:ID!,tag:ID!,user:ID!):Article!
+    "Rename an article you own"
+    renameArticle(article: ID!, title: String!, user: ID!): Article!
 
-  "add a user to an article"
-  shareArticle(article:ID!,to:ID!,user:ID!):Article!
+    "Change the zoteroLink to an article"
+    zoteroArticle(article: ID!, zotero: String!, user: ID!): Article!
 
-  "Send last version, or specified version, to an user"
-  sendArticle(article:ID!,version:ID,to:ID!,user:ID!):Article! # this is a copy of last version OR specified version
-  # fork is sendArticle to yourself
+    "Remove from owners"
+    deleteArticle(article: ID!, user: ID!): Article!
+  }
 
-  "Rename an article you own"
-  renameArticle(article:ID!,title:String!,user:ID!):Article!
-
-  "Change the zoteroLink to an article"
-  zoteroArticle(article:ID!,zotero:String!,user:ID!):Article!
-
-  "Remove from owners"
-  deleteArticle(article:ID!,user:ID!):Article!
-}
-
-schema {
+  schema {
     query: RootQuery
     mutation: RootMutation
-}
-`);
+  }
+`)
