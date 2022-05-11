23a50078d6f2499986e8d3a0bd8624c1669803cd
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2022-05-11 15:51:27 +0200

    Permet de gérer les exceptions dans les routes async d'Express

--- export/src/app.js
+++ export/src/app.js
@@ -7,6 +7,14 @@ const pino = require('pino-http')({
 
 const { exportArticleHtml, exportArticleZip, exportBookHtml, exportBookZip, exportVersionHtml, exportVersionZip, exportBatchTagZip } = require('./export.js')
 
+// inspired by: https://github.com/Abazhenov/express-async-handler/blob/master/index.js
+const asyncHandler = fn =>
+  function asyncUtilWrap (...args) {
+    const fnReturn = fn(...args)
+    const next = args[args.length - 1]
+    return Promise.resolve(fnReturn).catch(next)
+  }
+
 const app = express()
 app.use(pino)
 app.use(cors({
@@ -15,24 +23,28 @@ app.use(cors({
 
 const listenPort = process.env.PORT || 3060
 
+const asyncExportVersionHtml = asyncHandler(exportVersionHtml)
+const asyncExportArticleHtml = asyncHandler(exportArticleHtml)
+const asyncExportBookHtml = asyncHandler(exportBookHtml)
+
 const exportRouter = express.Router()
-exportRouter.get('/version/:id/html', exportVersionHtml)
-exportRouter.get('/version/:id/zip', exportVersionZip)
-exportRouter.get('/article/:id/html', exportArticleHtml)
-exportRouter.get('/article/:id/zip', exportArticleZip)
-exportRouter.get('/book/:id/html', exportBookHtml)
-exportRouter.get('/book/:id/zip', exportBookZip)
-exportRouter.get('/tag/:ids/zip', exportBatchTagZip)
+exportRouter.get('/version/:id/html', asyncExportVersionHtml)
+exportRouter.get('/version/:id/zip', asyncHandler(exportVersionZip))
+exportRouter.get('/article/:id/html', asyncExportArticleHtml)
+exportRouter.get('/article/:id/zip', asyncHandler(exportArticleZip))
+exportRouter.get('/book/:id/html', asyncExportBookHtml)
+exportRouter.get('/book/:id/zip', asyncHandler(exportBookZip))
+exportRouter.get('/tag/:ids/zip', asyncHandler(exportBatchTagZip))
 app.use('/export', exportRouter)
 
 // WARNING: the following routes are used by article/book preview routes to annotate the page.
 // the URL is used as an unique identifier and should not be modified otherwise the annotations will be "lost"!
 // as a result, we are forced to keep these alias until we use an URL-independent identifier on Hypothesis.
 // see: https://web.hypothes.is/help/how-hypothesis-interacts-with-document-metadata/
-app.get('/api/v1/htmlVersion/:id', exportVersionHtml)
-app.get('/api/v1/htmlArticle/:id', exportArticleHtml)
-app.get('/htmlBook/:id', exportBookHtml)
-app.get('/api/v1/htmlBook/:id', exportBookHtml)
+app.get('/api/v1/htmlVersion/:id', asyncExportVersionHtml)
+app.get('/api/v1/htmlArticle/:id', asyncExportArticleHtml)
+app.get('/htmlBook/:id', asyncExportBookHtml)
+app.get('/api/v1/htmlBook/:id', asyncExportBookHtml)
 
 app.use(function errorHandler (error, req, res, next) {
   if (!error) {
