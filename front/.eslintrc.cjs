bab87d54e7deea79259ca649df98b17aa4c48127
Thomas Parisot <thom4parisot@users.noreply.github.com>
2022-12-15 20:14:03 -0500

    Catch schema formatting error with eslint rules

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -1,31 +1,42 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jest/globals": true,
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsonc/recommended-with-json',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 13,
-        "sourceType": "module"
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ["**/*.json", "**/*.json5", "**/*.jsonc"],
+      parser: "jsonc-eslint-parser",
     },
-    "plugins": [
-        "react",
-        "jest"
-    ],
-    "settings": {
-      "react": {
-        "version": "16.13"
-      }
+  ],
+  plugins: ['react', 'jest'],
+  settings: {
+    react: {
+      version: '16.13',
     },
-    "rules": {
-      'react/prop-types': ['warn'],
-      'no-unused-vars': ['warn']
-    }
-};
+  },
+  rules: {
+    'react/prop-types': ['warn'],
+    'no-unused-vars': ['warn'],
+    'jsonc/indent': ['error', 2],
+    // 'jsonc/sort-keys': ['warn'],
+    'jsonc/key-spacing': ['error'],
+    'jsonc/no-irregular-whitespace': ['error'],
+    'jsonc/object-curly-newline': ['error'],
+    'jsonc/object-property-newline': ['error']
+  },
+}
