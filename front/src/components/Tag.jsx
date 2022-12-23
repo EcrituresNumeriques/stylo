44b4d4073d31a8b5dd36b0cd266e9f011c7bdbe8
Thomas Parisot <thom4parisot@users.noreply.github.com>
2022-12-23 13:17:12 -0500

    Create an article with nested mutation query

--- front/src/components/Tag.jsx
+++ front/src/components/Tag.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import styles from './tag.module.scss'
 
-export default function ArticleTag ({ tag, key, onClick, disableAction }) {
+export default function ArticleTag ({ tag, selected, key, onClick, disableAction }) {
   const classNames = [
     styles.tag,
-    tag.selected ? styles.selected : null
+    (tag.selected || selected) ? styles.selected : null
   ].join(' ')
 
-  const color = tag.color || 'grey'
+  const backgroundColor = tag.color || 'grey'
 
-  console.log('disableAction', disableAction)
   return (<label className={classNames}>
-    { !disableAction && <input name={key} type="checkbox" checked={tag.selected} onChange={onClick} />}
+    { !disableAction && <input name={key} value={tag._id} type="checkbox" checked={tag.selected || selected} onChange={onClick} />}
     {tag.name}
-    <span className={styles.chip} style={{backgroundColor: color}}/>
+    <span className={styles.chip} style={{ backgroundColor }}/>
   </label>)
 }
