EcrituresNumeriques/stylo

commit 7d19a7bfbf449a38082ba1812e9f1b80ee8efd61

Transition des requêtes GraphQL vers un token de session (#483)
Co-authored-by: Thomas Parisot <oncletom@users.noreply.github.com>
Co-authored-by: Thomas Parisot <thom4parisot@users.noreply.github.com>

front/src/components/Write/bibliographe/Bibliographe.jsx
@@ -1,53 +1,40 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react'
-import { connect, useDispatch } from 'react-redux'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 import debounce from 'lodash.debounce'
+import { Check, Plus, Trash } from 'react-feather'
 
 import styles from './bibliographe.module.scss'
 import etv from '../../../helpers/eventTargetValue'
-import { getUserProfile } from '../../../helpers/userProfile'
-import askGraphQL from '../../../helpers/graphQL'
+import { useProfile } from '../../../helpers/userProfile'
+import { useGraphQL } from '../../../helpers/graphQL'
 import { fetchAllCollectionsPerLibrary, fetchBibliographyFromCollectionHref } from '../../../helpers/zotero'
 import { toBibtex, toEntries, validate } from '../../../helpers/bibtex'
 import ReferenceTypeIcon from '../../ReferenceTypeIcon'
 import Button from '../../Button'
 import Field from '../../Field'
 
-import { Check, Plus, Trash } from 'react-feather'
 import Select from '../../Select'
 import NavTag from '../../NavTab'
 
-import BibliographyService from '../../../services/BibliographyService'
-
-const mapStateToProps = ({ workingArticle, activeUser, applicationConfig }) => {
-  return { workingArticleBibliography: workingArticle.bibliography, activeUser, applicationConfig }
-}
-
-const mapDispatchToProps = (dispatch) => ({
-  refreshProfile: (applicationConfig) =>
-    getUserProfile(applicationConfig).then((response) =>
-      dispatch({ type: 'PROFILE', ...response })
-    ),
-})
-
-function ConnectedBibliographe({ article, cancel, refreshProfile, workingArticleBibliography, activeUser, applicationConfig }) {
-  const { backendEndpoint } = applicationConfig
+export default function Bibliographe({ article, cancel }) {
   const [selector, setSelector] = useState('zotero')
   const [isSaving, setSaving] = useState(false)
   const [bib, setBib] = useState(workingArticleBibliography.text)
   const [bibTeXEntries, setBibTeXEntries] = useState(workingArticleBibliography.entries)
   const [addCitation, setAddCitation] = useState('')
-  const [citationValidationResult, setCitationValidationResult] = useState({
-    valid: false,
-  })
-  const [rawBibTeXValidationResult, setRawBibTeXValidationResult] = useState({
-    valid: false,
-  })
+  const [citationValidationResult, setCitationValidationResult] = useState({ valid: false })
+  const [rawBibTeXValidationResult, setRawBibTeXValidationResult] = useState({ valid: false })
   const [zoteroLink, setZoteroLink] = useState(article.zoteroLink || '')
   const [zoteroCollectionHref, setZoteroCollectionHref] = useState(null)
-  const { zoteroToken } = activeUser
+  const workingArticleBibliography = useSelector(state => state.workingArticle.bibliography, shallowEqual)
+  const backendEndpoint = useSelector(state => state.applicationConfig.backendEndpoint)
+  const zoteroToken = useSelector(state => state.activeUser.zoteroToken)
+  const userId = useSelector(state => state.activeUser._id)
+  const runQuery = useGraphQL()
   const [zoteroCollections, setZoteroCollections] = useState({})
   const citationForm = useRef()
   const dispatch = useDispatch()
+  const refreshProfile = useProfile()
 
   useEffect(() => {
     if (zoteroToken) {
@@ -121,15 +108,10 @@ function ConnectedBibliographe({ article, cancel, refreshProfile, workingArticle
         const query = `mutation($user:ID!,$article:ID!,$zotero:String!){zoteroArticle(article:$article,zotero:$zotero,user:$user){ _id zoteroLink}}`
         const variables = {
           zotero: zoteroLink,
-          user: activeUser._id,
+          user: userId,
           article: article._id,
         }
-        await askGraphQL(
-          { query, variables },
-          'updating zoteroLink',
-          null,
-          applicationConfig
-        )
+        await runQuery({ query, variables })
       } catch (err) {
         alert(err)
       } finally {
@@ -269,7 +251,7 @@ function ConnectedBibliographe({ article, cancel, refreshProfile, workingArticle
                   )
                   const intervalId = setInterval(() => {
                     if (popup.closed) {
-                      refreshProfile(applicationConfig)
+                      refreshProfile()
                       clearInterval(intervalId)
                     }
                   }, 1000)
@@ -416,10 +398,3 @@ function ConnectedBibliographe({ article, cancel, refreshProfile, workingArticle
     </article>
   )
 }
-
-const Bibliographe = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ConnectedBibliographe)
-
-export default Bibliographe

front/src/createReduxStore.js
@@ -5,6 +5,8 @@ import ArticleService from "./services/ArticleService"
 import MetadataService from "./services/MetadataService"
 import BibliographyService from "./services/BibliographyService";
 
+const { SNOWPACK_SESSION_STORAGE_ID: sessionTokenName='sessionToken' } = import.meta.env
+
 function createReducer (initialState, handlers) {
   return function reducer (state = initialState, action) {
     if (Object.prototype.hasOwnProperty.call(handlers, action.type)) {
@@ -17,9 +19,8 @@ function createReducer (initialState, handlers) {
 
 // Définition du store Redux et de l'ensemble des actions
 const initialState = {
-  logedIn: false,
   hasBooted: false,
-  sessionToken: undefined,
+  sessionToken: localStorage.getItem(sessionTokenName),
   workingArticle: {
     state: 'saved',
     bibliography: {
@@ -64,6 +65,7 @@ const reducer = createReducer(initialState, {
   PROFILE: setProfile,
   CLEAR_ZOTERO_TOKEN: clearZoteroToken,
   LOGIN: loginUser,
+  UPDATE_SESSION_TOKEN: setSessionToken,
   UPDATE_ACTIVE_USER: updateActiveUser,
   LOGOUT: logoutUser,
 
@@ -90,20 +92,20 @@ const createNewArticleVersion = store => {
   return next => {
     return async (action) => {
       if (action.type === 'CREATE_NEW_ARTICLE_VERSION') {
-        const { articleVersions, activeUser, applicationConfig } = store.getState()
+        const { articleVersions, activeUser, sessionToken, applicationConfig } = store.getState()
         const userId = activeUser._id
         const { articleId, major, message } = action
-        const versionService = new VersionService(userId, articleId, applicationConfig)
+        const versionService = new VersionService(userId, articleId, sessionToken, applicationConfig)
         const response = await versionService.createNewArticleVersion(major, message)
         store.dispatch({ type: 'SET_ARTICLE_VERSIONS', versions: [response.saveVersion, ...articleVersions] })
         return next(action)
       }
       if (action.type === 'UPDATE_WORKING_ARTICLE_TEXT') {
-        const { activeUser, applicationConfig } = store.getState()
+        const { activeUser, sessionToken, applicationConfig } = store.getState()
         const userId = activeUser._id
         const { articleId, text } = action
         try {
-          const { updateWorkingVersion } = await new ArticleService(userId, articleId, applicationConfig).saveText(text)
+          const { updateWorkingVersion } = await new ArticleService(userId, articleId, sessionToken, applicationConfig).saveText(text)
           store.dispatch({ type: 'SET_WORKING_ARTICLE_STATE', workingArticleState: 'saved' })
           store.dispatch({ type: 'SET_WORKING_ARTICLE_TEXT', text })
           store.dispatch({ type: 'SET_WORKING_ARTICLE_UPDATED_AT', updatedAt: updateWorkingVersion.updatedAt })
@@ -118,11 +120,11 @@ const createNewArticleVersion = store => {
         return next(action)
       }
       if (action.type === 'UPDATE_WORKING_ARTICLE_METADATA') {
-        const { activeUser, applicationConfig } = store.getState()
+        const { activeUser, sessionToken, applicationConfig } = store.getState()
         const userId = activeUser._id
         const { articleId, metadata } = action
         try {
-          const { updateWorkingVersion } = await new MetadataService(userId, articleId, applicationConfig).saveMetadata(metadata)
+          const { updateWorkingVersion } = await new MetadataService(userId, articleId, sessionToken, applicationConfig).saveMetadata(metadata)
           store.dispatch({ type: 'SET_WORKING_ARTICLE_STATE', workingArticleState: 'saved' })
           store.dispatch({ type: 'SET_WORKING_ARTICLE_METADATA', metadata })
           store.dispatch({ type: 'SET_WORKING_ARTICLE_UPDATED_AT', updatedAt: updateWorkingVersion.updatedAt })
@@ -182,6 +184,18 @@ function persistStateIntoLocalStorage ({ getState }) {
         document.location.replace(applicationConfig.backendEndpoint + '/logout')
       }
 
+      if (action.type === 'LOGIN' || action.type === 'UPDATE_SESSION_TOKEN') {
+        next(action)
+        const { sessionToken } = getState()
+        localStorage.setItem(sessionTokenName, sessionToken)
+        return
+      }
+
+      if (action.type === 'LOGOUT') {
+        localStorage.removeItem(sessionTokenName)
+        return next(action)
+      }
+
       return next(action)
     }
   }
@@ -205,7 +219,6 @@ function setProfile (state, action) {
   return Object.assign({}, state, {
     hasBooted: true,
     activeUser,
-    sessionToken: activeUser.apiToken,
     logedIn: true,
   })
 }
@@ -216,18 +229,24 @@ function clearZoteroToken (state) {
   return state
 }
 
-function loginUser (state, { login }) {
-  if (login.user && login.token) {
+function setSessionToken (state, { token: sessionToken }) {
+  return {
+    ...state,
+    sessionToken
+  }
+}
+
+function loginUser (state, { user, token:sessionToken }) {
+  if (sessionToken) {
     return {
       ...state,
-      logedIn: true,
+      sessionToken,
       activeUser: {
-        ...login.user,
+        ...user,
         // dates are expected to be in timestamp string format (including milliseconds)
-        createdAt: String(new Date(login.user.createdAt).getTime()),
-        updatedAt: String(new Date(login.user.updatedAt).getTime()),
+        createdAt: String(new Date(user.createdAt).getTime()),
+        updatedAt: String(new Date(user.updatedAt).getTime()),
       },
-      sessionToken: login.token,
     }
   }
 

.github/workflows/node.yml
@@ -29,7 +29,7 @@ jobs:
       with:
         node-version: ${{ matrix.node-version }}
     - name: Install and test
-      run: npm clean-install-test --prefix front
+      run: npm clean-install-test --prefix front --no-audit --no-fund
     - name: Build front
       run: npm run build --prefix front
 
@@ -45,7 +45,7 @@ jobs:
       with:
         node-version: ${{ matrix.node-version }}
     - name: Install
-      run: npm clean-install --prefix graphql
+      run: npm clean-install --prefix graphql --no-audit --no-fund
 
   build-export:
     runs-on: ubuntu-latest
@@ -59,4 +59,4 @@ jobs:
         with:
           node-version: ${{ matrix.node-version }}
       - name: Install and test
-        run: npm clean-install-test --prefix export
+        run: npm clean-install-test --prefix export --no-audit --no-fund

docker-compose.yaml
@@ -14,6 +14,8 @@ services:
       test: ["CMD", "mongo", "--eval", "\"db.adminCommand('ping')\""]
       interval: 30s
       timeout: 5s
+    ports:
+      - 127.0.0.1:27017:27017
   graphql-stylo:
     build: ./graphql/
     container_name: "graphql-stylo"

ecosystem.config.js
@@ -4,7 +4,8 @@ module.exports = {
   apps: [
     {
       name: "front",
-      script: "vite",
+      script: "npm",
+      args: "start",
       cwd: "./front",
       env: parsed
     },
@@ -17,7 +18,8 @@ module.exports = {
       ignore_watch: ['node_modules'],
       env: {
         NODE_ENV: 'dev',
-        CHOKIDAR_USEPOLLING: 1
+        CHOKIDAR_USEPOLLING: 1,
+        ...parsed
       }
     },
     {
@@ -29,7 +31,8 @@ module.exports = {
       ignore_watch: ['node_modules'],
       env: {
         NODE_ENV: 'dev',
-        CHOKIDAR_USEPOLLING: 1
+        CHOKIDAR_USEPOLLING: 1,
+        ...parsed
       }
     }
   ]

front/src/components/AcquintanceAddForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useCallback } from 'react'
-import { shallowEqual, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import formStyles from './field.module.scss'
+import { useGraphQL } from '../helpers/graphQL.js'
 
 import Button from './Button'
 import Field from './Field'
@@ -10,8 +11,8 @@ import AcquintanceService from '../services/AcquintanceService'
 export default function ConnectedAcquintances ({ onAdd }) {
   const [contact, setContact] = useState('')
   const userId = useSelector(state => state.activeUser._id)
-  const applicationConfig = useSelector(state => state.applicationConfig, shallowEqual)
-  const acquintanceService = new AcquintanceService(userId, applicationConfig)
+  const runQuery = useGraphQL()
+  const acquintanceService = new AcquintanceService(userId, runQuery)
 
   const addContact = useCallback(async (event) => {
     event.preventDefault()
@@ -26,6 +27,8 @@ export default function ConnectedAcquintances ({ onAdd }) {
   return (<form onSubmit={addContact} className={formStyles.inlineFields}>
     <Field
       autoFocus={true}
+      required={true}
+      type="email"
       className={formStyles.fullWidth}
       placeholder='Email of the contact you want to add'
       value={contact}

front/src/components/Acquintances.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import { shallowEqual, useSelector } from 'react-redux'
 import { Send, UserMinus, UserPlus } from 'react-feather'
 
 import styles from './acquintances.module.scss'
@@ -9,47 +9,45 @@ import AcquintanceAddForm from './AcquintanceAddForm'
 import Button from './Button'
 
 import AcquintanceService from '../services/AcquintanceService'
+import { useGraphQL } from '../helpers/graphQL'
 
-const mapStateToProps = ({ activeUser, applicationConfig }) => {
-  return { activeUser, applicationConfig }
-}
-
-function ConnectedAcquintances ({ article, activeUser, setNeedReload, cancel, applicationConfig }) {
+export default function Acquintances ({ article, setNeedReload, cancel }) {
   const [acquintances, setAcquintances] = useState([])
   const [loading, setLoading] = useState(true)
   const [contributors, setContributors] = useState(article.contributors)
-  const articleId = article._id
+  const activeUser = useSelector(state => state.activeUser, shallowEqual)
   const userId = activeUser._id
-  const acquintanceService = new AcquintanceService(userId, applicationConfig)
+  const runQuery = useGraphQL()
+  const acquintanceService = new AcquintanceService(userId, runQuery)
 
   const sharedAccountsIds = activeUser.permissions.map(({ user }) => user._id)
   const contributorsIds = contributors.map(({ user }) => user._id)
 
   const shareArticle = async (to) => {
     try {
-      const { shareArticle:article } = await acquintanceService.shareArticle(articleId, to)
-      setContributors(article.contributors)
+      const { shareArticle } = await acquintanceService.shareArticle(article._id, to)
+      setContributors(shareArticle.contributors)
     } catch (err) {
-      console.error(`Unable to share article ${articleId} with ${to} (userId: ${userId})`, err)
+      console.error(`Unable to share article ${article._id} with ${to} (userId: ${userId})`, err)
       alert(err)
     }
   }
 
   const unshareArticle = async (to) => {
     try {
-      const { unshareArticle:article } = await acquintanceService.unshareArticle(articleId, to)
-      setContributors(article.contributors)
+      const { unshareArticle } = await acquintanceService.unshareArticle(article._id, to)
+      setContributors(unshareArticle.contributors)
     } catch (err) {
-      console.error(`Unable to unshare article ${articleId} with ${to} (userId: ${userId})`, err)
+      console.error(`Unable to unshare article ${article._id} with ${to} (userId: ${userId})`, err)
       alert(err)
     }
   }
 
   const duplicateArticle = async (to) => {
     try {
-      await acquintanceService.duplicateArticle(articleId, to)
+      await acquintanceService.duplicateArticle(article._id, to)
     } catch (err) {
-      console.error(`Unable to duplicate article ${articleId} with ${to} (userId: ${userId})`, err)
+      console.error(`Unable to duplicate article ${article._id} with ${to} (userId: ${userId})`, err)
       alert(err)
     }
     setNeedReload()
@@ -96,6 +94,3 @@ function ConnectedAcquintances ({ article, activeUser, setNeedReload, cancel, ap
     </section>
   )
 }
-
-const Acquintances = connect(mapStateToProps)(ConnectedAcquintances)
-export default Acquintances

front/src/components/Article.jsx
@@ -14,13 +14,13 @@ import ArticleTags from './ArticleTags'
 import formatTimeAgo from '../helpers/formatTimeAgo'
 import { generateArticleExportId } from "../helpers/identifier"
 import etv from '../helpers/eventTargetValue'
-import askGraphQL from '../helpers/graphQL'
 
 import Field from './Field'
 import Button from './Button'
 import { Check, ChevronDown, ChevronRight, Copy, Edit3, Eye, Printer, Share2, Trash } from 'react-feather'
 
 import AcquintanceService from '../services/AcquintanceService'
+import { useGraphQL } from '../helpers/graphQL'
 
 export default function Article ({ article, currentUser:activeUser, setNeedReload, updateTitleHandler, updateTagsHandler, masterTags }) {
   const [expanded, setExpanded] = useState(false)
@@ -31,12 +31,10 @@ export default function Article ({ article, currentUser:activeUser, setNeedReloa
   const [title, setTitle] = useState(article.title)
   const [tempTitle, setTempTitle] = useState(article.title)
   const [sharing, setSharing] = useState(false)
-
-  const applicationConfig = useSelector(state => state.applicationConfig, shallowEqual)
-  const sessionToken = useSelector(state => state.sessionToken)
+  const runQuery = useGraphQL()
 
   const isArticleOwner = activeUser._id === article.owner._id
-  const acquintanceService = new AcquintanceService(activeUser._id, applicationConfig)
+  const acquintanceService = new AcquintanceService(activeUser._id, runQuery)
 
   const contributors = article.contributors.filter(c => c.user._id !== article.owner._id)
 
@@ -58,12 +56,7 @@ export default function Article ({ article, currentUser:activeUser, setNeedReloa
       article: article._id,
       title: tempTitle,
     }
-    await askGraphQL(
-      { query, variables },
-      'Renaming Article',
-      sessionToken,
-      applicationConfig
-    )
+    await runQuery({ query, variables })
     setTitle(tempTitle)
     setRenaming(false)
     if (updateTitleHandler) {

commit 9c8cebd74ddcab7222193443775adc3b66b1642a

Fix Bibliography service (regression from #483)

front/src/components/Write/bibliographe/Bibliographe.jsx
@@ -21,6 +21,7 @@ import { linkToZotero as query } from '../../Article.graphql'
 export default function Bibliographe({ article, cancel }) {
   const [selector, setSelector] = useState('zotero')
   const [isSaving, setSaving] = useState(false)
+  const workingArticleBibliography = useSelector(state => state.workingArticle.bibliography, shallowEqual)
   const [bib, setBib] = useState(workingArticleBibliography.text)
   const [bibTeXEntries, setBibTeXEntries] = useState(workingArticleBibliography.entries)
   const [addCitation, setAddCitation] = useState('')
@@ -28,7 +29,6 @@ export default function Bibliographe({ article, cancel }) {
   const [rawBibTeXValidationResult, setRawBibTeXValidationResult] = useState({ valid: false })
   const [zoteroLink, setZoteroLink] = useState(article.zoteroLink || '')
   const [zoteroCollectionHref, setZoteroCollectionHref] = useState(null)
-  const workingArticleBibliography = useSelector(state => state.workingArticle.bibliography, shallowEqual)
   const backendEndpoint = useSelector(state => state.applicationConfig.backendEndpoint)
   const zoteroToken = useSelector(state => state.activeUser.zoteroToken)
   const userId = useSelector(state => state.activeUser._id)

front/src/createReduxStore.js
@@ -133,11 +133,11 @@ const createNewArticleVersion = store => {
         return next(action)
       }
       if (action.type === 'UPDATE_WORKING_ARTICLE_BIBLIOGRAPHY') {
-        const { activeUser, applicationConfig } = store.getState()
+        const { activeUser, sessionToken, applicationConfig } = store.getState()
         const userId = activeUser._id
         const { articleId, bibliography } = action
         try {
-          const { updateWorkingVersion } = await new ArticleService(userId, articleId, applicationConfig).saveBibliography(bibliography)
+          const { updateWorkingVersion } = await new ArticleService(userId, articleId, sessionToken, applicationConfig).saveBibliography(bibliography)
           store.dispatch({ type: 'SET_WORKING_ARTICLE_STATE', workingArticleState: 'saved' })
           store.dispatch({ type: 'SET_WORKING_ARTICLE_BIBLIOGRAPHY', bibliography })
           store.dispatch({ type: 'SET_WORKING_ARTICLE_UPDATED_AT', updatedAt: updateWorkingVersion.updatedAt })