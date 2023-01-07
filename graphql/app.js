EcrituresNumeriques/stylo

commit e3de56a40f287958b4a2dfdf697e5becf6d4c224

Refactoring de `live` en `workingVersion` (#458)
Co-authored-by: Thomas Parisot <oncletom@users.noreply.github.com>
Co-authored-by: Guillaume Grossetie <ggrossetie@gmail.com>
Co-authored-by: Guillaume Grossetie <g.grossetie@gmail.com>

graphql/data/defaultsData.js
@@ -62,7 +62,14 @@ ordseq: ''
 ---`,
   title:'New article',
   sommaire:'',
-  md:'',
+  bib: '',
+  md: `## Section Title
+
+Add your text here, and the document title in the metadatas tool.
+
+1. Use only title of level 2 (\`##\`) for sections and more (level 1 is used for the article title)
+2. The text is automatically saved. You can version it with \`[Save as]\` button
+3. Check the documentation in the menu`,
   message:'Genesis',
   article: {
       title:'How to Stylo',
@@ -309,7 +316,7 @@ abstract:
       complète est accessible en cliquant sur le lien documentation.
     text_f: >-
       C'est article est un exemple d'article type édité sur _Stylo_. _Stylo_ est
-      un éditeur d'article scientifique dédié aux sciences humaines. 
+      un éditeur d'article scientifique dédié aux sciences humaines.
 
       Vous pouvez éditer cet article pour vous entraîner. Une documentation plus
       complète est accessible en cliquant sur le lien documentation.

graphql/models/user.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 const defaultsData = require('../data/defaultsData')
 
 const Article = require('./article');
-const Version = require('./version');
-
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -69,29 +67,20 @@ const userSchema = new Schema({
 }, {timestamps: true});
 
 module.exports = mongoose.model('User', userSchema)
+module.exports.schema = userSchema
+
 module.exports.postCreate = function postCreate (newUser) {
   console.log('%s has been saved', newUser._id);
 
   // Create a new default article for each new user
   const defaultArticle = defaultsData.article
   const newArticle = new Article({title: defaultArticle.title});
-  const newVersion = new Version({
-    md: defaultArticle.md,
-    yaml: defaultArticle.yaml,
-    bib: defaultArticle.bib,
-    sommaire: defaultArticle.sommaire
-  });
 
-  newArticle.versions.push(newVersion)
-  newVersion.article = newArticle
-  newVersion.owner = newUser
-  
   newUser.articles.push(newArticle)
   newArticle.owners.push(newUser)
-  
+
   return Promise.all([
     newUser.save(),
     newArticle.save(),
-    newVersion.save(),
   ])
 }

graphql/resolvers/articleResolver.js
@@ -26,20 +26,18 @@ module.exports = {
       if(!thisUser){throw new Error('This user does not exist')}
 
       //Add default article + default version
-      const newArticle = new Article({title:args.title || defaultsData.title});
-      const newVersion = new Version({md:defaultsData.md,yaml:thisUser.yaml || defaultsData.yaml,bib:'',sommaire:defaultsData.sommaire,message:defaultsData.message});
-      newArticle.versions.push(newVersion)
-      newVersion.article = newArticle
+      const newArticle = new Article({
+        title: args.title || defaultsData.title
+      });
 
       thisUser.articles.push(newArticle)
       newArticle.owners.push(thisUser)
 
       const createdArticle = await newArticle.save();
-      await newVersion.save();
       await thisUser.save();
 
       //Save the article and version ID in the req object, for other resolver to consum with "new" ID
-      req.created = {...req.created,article:createdArticle.id,version:newVersion.id}
+      req.created = {...req.created, article:createdArticle.id}
 
       // TODO: filter owners vers ID
       createdArticle.owners = createdArticle.owners.map(o => o.id)
@@ -48,8 +46,35 @@ module.exports = {
     catch(err){
       throw err
     }
-
   },
+
+  updateWorkingVersion: async (args, {req}) => {
+    const ALLOWED_PARAMS = ['bib', 'md', 'yaml']
+
+    args = populateArgs(args,req)
+    isUser(args,req)
+
+    //fetch user
+    const thisUser = await User.findOne({_id: args.user})
+    if(!thisUser){throw new Error('This user does not exist')}
+
+    //fetch article
+    const fetchedArticle = await Article.findOne({_id: args.article}).populate('owners');
+    if(!fetchedArticle){throw new Error('Wrong article ID')}
+
+    if(!fetchedArticle.owners.map(u => u.id).includes(thisUser.id)){
+      throw new Error('User has no right to push new version')
+    }
+
+    ALLOWED_PARAMS
+      .filter(key => key in args)
+      .forEach(key => fetchedArticle.workingVersion[key] = args[key])
+
+    const returnArticle = await fetchedArticle.save()
+
+    return populateArticle(returnArticle)
+  },
+
   shareArticle: async (args,{req}) => {
     try{
       populateArgs(args,req)
@@ -90,23 +115,8 @@ module.exports = {
       const fetchedUser = await User.findOne({_id:args.to})
       if(!fetchedUser){throw new Error('Unable to find user')}
 
-      let fetchedVersion
-      if(args.version){
-        fetchedVersion = await Version.findOne({_id:args.version,article:args.article})
-      }
-      else {
-        fetchedVersion = await Version.findOne({_id:fetchedArticle.versions[fetchedArticle.versions.length-1].toString()})
-      }
       if(!fetchedArticle){throw new Error('Unable to fetch version')}
 
-      //clone version
-
-      let newVersion = new Version({
-        md:fetchedVersion.md,
-        sommaire:fetchedVersion.sommaire,
-        yaml:fetchedVersion.yaml,
-        bib:fetchedVersion.bib
-      });
       //All good, create new Article & merge version/article/user
       let prefix = '[Sent] '
       if(args.user === args.to){
@@ -115,14 +125,11 @@ module.exports = {
 
       let newArticle = new Article({title:prefix+fetchedArticle.title, zoteroLink:fetchedArticle.zoteroLink})
       newArticle.owners.push(fetchedUser.id)
-      newArticle.versions.push(newVersion)
       fetchedUser.articles.push(newArticle)
-      newVersion.owner = fetchedUser
 
       //Save the three objects
       const returnedArticle = await newArticle.save()
       await fetchedUser.save()
-      await newVersion.save()
 
       return populateArticle(returnedArticle)
 

graphql/resolvers/userResolver.js
@@ -51,10 +51,7 @@ module.exports = {
 
     //Add default article + default version
     const defaultArticle = defaultsData.article
-    const newArticle = new Article({title:defaultArticle.title});
-    const newVersion = new Version({md:defaultArticle.md,yaml:defaultArticle.yaml,bib:defaultArticle.bib,sommaire:defaultArticle.sommaire});
-    newArticle.versions.push(newVersion)
-    newVersion.article = newArticle
+    const newArticle = new Article({ title:defaultArticle.title });
 
     newUser.articles.push(newArticle)
     newArticle.owners.push(newUser)
@@ -63,11 +60,9 @@ module.exports = {
     const createdUser = await newUser.save();
     await newPassword.save();
     await newArticle.save();
-    newVersion.owner = createdUser
-    await newVersion.save();
 
     //Save the user/article/version/password ID in the req object, for other resolver to consum with "new" ID
-    req.created = {...req.created,article:newArticle.id,user:createdUser.id,version:newVersion.id,password:newPassword.id}
+    req.created = {...req.created,article:newArticle.id,user:createdUser.id,password:newPassword.id}
 
     return populateUser(createdUser)
   },

export/src/export.js
@@ -176,10 +176,9 @@ const getBookById = async (bookId) => {
 
 const getArticleExportContext = async (articleId) => {
   const article = await getArticleById(articleId)
-  const latestVersionId = article._doc.versions.pop()
-  const latestVersion = await getVersionById(latestVersionId)
-  const { bib, yaml, md } = latestVersion._doc
-  return { bib, yaml, md, id: articleId, versionId: latestVersionId, title: article._doc.title }
+  const latestVersion = article.workingVersion
+  const { bib, yaml, md } = latestVersion
+  return { bib, yaml, md, articleId, title: article._doc.title }
 }
 
 const getBookExportContext = async (bookId) => {
@@ -230,7 +229,7 @@ module.exports = {
     try {
       const articleId = req.params.id
       const articleExportContext = await getArticleExportContext(articleId)
-      await exportHtml(articleExportContext, res, req)
+      exportHtml({ ...articleExportContext, id: articleExportContext.articleId }, res, req)
     } catch (err) {
       errorHandler(err, res)
     }
@@ -239,25 +238,53 @@ module.exports = {
     try {
       const articleId = req.params.id
       const articleExportContext = await getArticleExportContext(articleId)
-      exportZip(articleExportContext, res, req)
+      exportZip({ ...articleExportContext, id: articleExportContext.articleId }, res, req)
     } catch (err) {
       errorHandler(err, res)
     }
   },
   exportVersionHtml: async (req, res, _) => {
     try {
-      const version = await getVersionById(req.params.id)
-      const { bib, yaml, md, _id: id } = version._doc
-      await exportHtml({ bib, yaml, md, id, title: id }, res, req)
+      const identifier = req.params.id
+      const [articleId, versionId] = identifier.includes(':')
+        ? identifier.split(':')
+        : [undefined, identifier]
+
+      if (versionId === 'latest' && articleId) {
+        const articleExportContext = await getArticleExportContext(articleId)
+        exportHtml(
+          { ...articleExportContext, id: articleExportContext.articleId },
+          res,
+          req
+        )
+      } else {
+        const version = await getVersionById(versionId)
+        const { bib, yaml, md, _id: id } = version._doc
+        exportHtml({ bib, yaml, md, id, title: id }, res, req)
+      }
     } catch (err) {
       errorHandler(err, res)
     }
   },
   exportVersionZip: async (req, res, _) => {
     try {
-      const version = await getVersionById(req.params.id)
-      const { bib, yaml, md, _id: id } = version._doc
-      await exportZip({ bib, yaml, md, id, title: id }, res, req)
+      const identifier = req.params.id
+      const [articleId, versionId] = identifier.includes(':')
+        ? identifier.split(':')
+        : [undefined, identifier]
+
+      if (versionId === 'latest' && articleId) {
+        const articleExportContext = await getArticleExportContext(articleId)
+        exportZip(
+          { ...articleExportContext, id: articleExportContext.articleId },
+          res,
+          req
+        )
+      } else {
+        const version = await getVersionById(req.params.id)
+        const { bib, yaml, md, _id: id } = version._doc
+        exportZip({ bib, yaml, md, id, title: id }, res, req)
+      }
     } catch (err) {
       errorHandler(err, res)
     }
@@ -268,7 +295,7 @@ module.exports = {
       if (exportBookContext === null) {
         return res.status(200).send('')
       }
-      await exportHtml(exportBookContext, res, req)
+      exportHtml(exportBookContext, res, req)
     } catch (err) {
       errorHandler(err, res)
     }
@@ -277,7 +304,7 @@ module.exports = {
     try {
       const bookId = req.params.id
       const exportBookContext = await getBookExportContext(bookId)
-      await exportZip(exportBookContext, res, req)
+      exportZip(exportBookContext, res, req)
     } catch (err) {
       errorHandler(err, res)
     }
@@ -296,7 +323,9 @@ module.exports = {
       // add files
       articles.forEach((article) => {
         const filename = normalize(article._doc.title)
-        const { md, bib, yaml } = article._doc.versions[0]
+        const { md, bib, yaml } = article._doc.versions.length > 0
+          ? article._doc.versions[0]
+          : article._doc.workingVersion
         archive.append(Buffer.from(md), { name: `${filename}.md` })
         archive.append(Buffer.from(bib), { name: `${filename}.bib` })
         archive.append(Buffer.from(yaml), { name: `${filename}.yaml` })

export/src/models/article.js
@@ -11,11 +11,25 @@ const articleSchema = new Schema({
   title: {
     type:String,
     required:true,
-    default:"autocreated"
+    default: 'autocreated'
   },
   zoteroLink:{
     type:String,
-    default:""
+    default: ''
+  },
+  workingVersion: {
+    md: {
+      type: String,
+      default: ''
+    },
+    yaml: {
+      type: String,
+      default: ''
+    },
+    bib: {
+      type: String,
+      default: ''
+    },
   },
   versions:[
     {

export/src/models/version.js
@@ -10,10 +10,6 @@ const versionSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'Article'
   },
-  autosave:{
-    type:Boolean,
-    default:true
-  },
   title:{
     type:String,
     default:''
@@ -30,21 +26,12 @@ const versionSchema = new Schema({
     type:String,
     default:''
   },
-  md:{
-    type:String,
-    default:'# titre'
-  },
+  md: String,
+  yaml: String,
+  bib: String,
   sommaire:{
     type:String,
     default:'# titre'
-  },
-  yaml:{
-    type:String,
-    default:''
-  },
-  bib:{
-    type:String,
-    default:''
   },
 }, {timestamps:true});
 

front/gatsby/.babelrc
@@ -1,12 +1,0 @@
-{
-  "presets": [
-    "@babel/preset-react",
-    [
-      "@babel/preset-env",
-      {
-        "corejs": "3.8",
-        "useBuiltIns": "entry"
-      }
-    ]
-  ]
-}

commit a28fa7c301e334f428709fa2de2d2b1d6786ba10

Refactor default article creation as a Model method
It also solves a regression introduction in #458, specifically in e3de56a40f287958b4a2dfdf697e5becf6d4c224

fixes #639

fixes #688

graphql/app.js
@@ -22,7 +22,6 @@ const { verifCreds } = require('./resolvers/authResolver')
 
 const { createJWTToken, populateUserFromJWT } = require('./helpers/token')
 const User = require('./models/user')
-const { postCreate } = User
 
 const app = express()
 
@@ -117,7 +116,8 @@ passport.use('oidc', new OidcStrategy({
 
     try {
       // we populate a user with initial content
-      await user.save().then(postCreate)
+      await user.save()
+      await user.createDefaultArticle()
     } catch (err) {
       return done(`Unable to create a new user ${email}, cause:`, err)
     }

graphql/data/defaultsData.js
@@ -77,26 +77,6 @@ Add your text here, and the document title in the metadatas tool.
   message:'Genesis',
   article: {
       title:'How to Stylo',
-      sommaire:`## Introduction
-## Les titres
-### Titres de niveau 3
-## Syntaxe minimale
-### Gras et italique
-### Commentaire
-### Images
-### Listes
-## Appareil critique
-### Notes à bas de page
-### Les références
-### Les citations
-## Versions et export
-### Métadonnées
-### Preview et annotation
-### Export
-## Annotations sémantiques
-### Annotation dans un paragraphe
-### Annotation de plusieurs paragraphes
-## Bibliographie`,
       md: `## Introduction
 
 Stylo est un éditeur de texte scientifique. Pour faire vos premiers pas sur Stylo, commencez par éditer cet article.

graphql/models/user.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 
-const defaultsData = require('../data/defaultsData')
-
-const Article = require('./article')
+const { article: defaultArticle, yaml: defaultUserYaml } = require('../data/defaultsData')
 const { UserPermissionSchema } = require('./permission')
-const { logger } = require('../logger')
 
 const Schema = mongoose.Schema;
 
@@ -71,12 +68,27 @@ const userSchema = new Schema({
   },
   yaml: {
     type: String,
-    defaults: defaultsData.yaml
+    defaults: defaultUserYaml
   }
 }, { timestamps: true });
 
 userSchema.methods.comparePassword = function (password) {
   return bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.createDefaultArticle = async function createDefaultArticle () {
+  const newArticle = await this.model('Article').create({
+    title: defaultArticle.title,
+    owner: this,
+    workingVersion: {
+      yaml: newArticle.yaml,
+      bib: newArticle.bib,
+      md: newArticle.md
+    },
+  })
+
+  this.articles.push(newArticle)
+  return this.save()
 }
 
 userSchema.statics.findAllArticles = async function ({ userId, fromSharedUserId }) {
@@ -157,19 +169,3 @@ userSchema.statics.findAccountAccessArticles = function (user, role = 'read') {
 
 module.exports = mongoose.model('User', userSchema)
 module.exports.schema = userSchema
-
-module.exports.postCreate = function postCreate (newUser) {
-  logger.info({ userId: newUser._id }, 'User has been saved')
-
-  // Create a new default article for each new user
-  const defaultArticle = defaultsData.article
-  const newArticle = new Article({ title: defaultArticle.title })
-
-  newUser.articles.push(newArticle)
-  newArticle.owner = newUser
-
-  return Promise.all([
-    newUser.save(),
-    newArticle.save(),
-  ])
-}

graphql/resolvers/articleResolver.js
@@ -28,7 +28,7 @@ module.exports = {
       }
 
       //Add default article + default version
-      const newArticle = new Article({
+      const newArticle = Article.create({
         title: args.title || defaultsData.title,
         owner: user,
         workingVersion: {
@@ -37,8 +37,6 @@ module.exports = {
           yaml: defaultsData.yaml,
         }
       });
-
-      await newArticle.save()
 
       user.articles.push(newArticle)
       await user.save()

graphql/resolvers/userResolver.js
@@ -1,10 +1,7 @@
 const User = require('../models/user')
-const Article = require('../models/article')
 
 const isUser = require('../policies/isUser')
 const isAdmin = require('../policies/isAdmin')
-
-const defaultsData = require('../data/defaultsData')
 
 module.exports = {
   Mutation: {
@@ -16,7 +13,7 @@ module.exports = {
       }
 
       //Create user then password
-      const newUser = new User({
+      const newUser = User.create({
         email: userInput.email,
         displayName: userInput.displayName || userInput.username,
         institution: userInput.institution || null,
@@ -26,17 +23,9 @@ module.exports = {
         authType: 'local',
       })
 
-      //Add default article + default version
-      const defaultArticle = defaultsData.article
-      const newArticle = new Article({ title: defaultArticle.title })
+      await newUser.createDefaultArticle()
 
-      newUser.articles.push(newArticle)
-      newArticle.owner = newUser
-
-      const createdUser = await newUser.save()
-      await newArticle.save()
-
-      return createdUser
+      return newUser
     },
     async addAcquintance (_, args, context) {
       const { userId } = isUser(args, context)