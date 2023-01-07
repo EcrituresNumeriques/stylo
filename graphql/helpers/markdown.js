fd107b255737d25e707bd03b88c047ad639a5fc6
Thomas Parisot <thom4parisot@users.noreply.github.com>
2023-01-07 14:01:57 -0500

    Add Markdown and Version — tested — helpers

---
+++ graphql/helpers/markdown.js
@@ -0,0 +1,10 @@
+function deriveToc (md) {
+  return md
+    .split('\n')
+    .filter((line) => line.match(/^#+ /))
+    .join('\n')
+}
+
+module.exports = {
+  deriveToc
+}
---
+++ graphql/helpers/markdown.test.js
@@ -0,0 +1,21 @@
+const { deriveToc } = require('./markdown.js')
+
+describe('deriveToc', () => {
+  const emptyText = ''
+  const textWithOneHeadline = `
+# Hello World
+This is not a # headline
+#This one too (because there is no space in between)`
+  const textWithManyHeadlines = `
+# Hello World
+This is not a # headline
+## How are you?
+Well, and you?
+### Level 3
+Not 2, not 4, but 3.`
+  test('computeMajorVersion', () => {
+    expect(deriveToc(emptyText)).toEqual('')
+    expect(deriveToc(textWithOneHeadline)).toEqual('# Hello World')
+    expect(deriveToc(textWithManyHeadlines)).toEqual('# Hello World\n## How are you?\n### Level 3')
+  })
+})
