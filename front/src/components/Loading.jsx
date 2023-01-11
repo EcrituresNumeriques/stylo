aa00b2154b011651399a5332b8ad2884b84a2f1a
Thomas Parisot <thom4parisot@users.noreply.github.com>
2023-01-11 13:40:21 -0500

    Force hide elements with the hidden HTML attribute

--- front/src/components/Loading.jsx
+++ front/src/components/Loading.jsx
@@ -4,8 +4,8 @@ import { Loader } from 'react-feather'
 
 import styles from './loading.module.scss'
 
-export default function Loading ({ label = 'Loading', inline = false, size = 42 }) {
-  return (<div className={clsx(styles.loading, inline && styles.inline)}>
+export default function Loading ({ label = 'Loading', inline = false, size = 42, hidden = false }) {
+  return (<div className={clsx(styles.loading, inline && styles.inline)} hidden={hidden}>
     <Loader size={size} aria-hidden />
     {label}…
   </div>)
