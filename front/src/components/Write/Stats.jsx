e808b5614b2db4f55e16e5b41683d8738feff60a
Thomas Parisot <thom4parisot@users.noreply.github.com>
2023-01-12 16:31:12 -0500

    Remember panel toggling as user preference

--- front/src/components/Write/Stats.jsx
+++ front/src/components/Write/Stats.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react'
+import React, { useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { ChevronDown, ChevronRight } from 'react-feather'
 
 import menuStyles from './menu.module.scss'
 
-export default ({ stats }) => {
-  const [expand, setExpand] = useState(true)
+export default function WriteStats ({ stats }) {
+  const expand = useSelector(state => state.articlePreferences.expandStats)
+  const dispatch = useDispatch()
+  const toggleExpand = useCallback(() => dispatch({ type: 'ARTICLE_PREFERENCES_TOGGLE', key: 'expandStats' }), [])
 
   return (
     <section className={menuStyles.section}>
-      <h1 onClick={() => setExpand(!expand)}>
+      <h1 onClick={toggleExpand}>
         {expand ? <ChevronDown/> : <ChevronRight/>} Stats
       </h1>
       {expand && (
