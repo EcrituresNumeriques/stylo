4d75bd16a67c44c090b094f48b258012cbe437ff
Thomas Parisot <thom4parisot@users.noreply.github.com>
2023-01-17 20:15:00 -0500

    Factor Version in its own component
    And prevent comparing to itself.

--- front/src/components/Write/Versions.jsx
+++ front/src/components/Write/Versions.jsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useState } from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 import { ArrowLeft, ChevronDown, ChevronRight } from 'react-feather'
@@ -12,6 +13,81 @@ import Export from '../Export'
 import Button from '../Button'
 import CreateVersion from './CreateVersion'
 import formatTimeAgo from '../../helpers/formatTimeAgo.js'
+import clsx from 'clsx'
+
+function Version ({ articleId, compareTo, onExport, selectedVersion, v }) {
+  const className = clsx({
+    [styles.selected]: v._id === selectedVersion,
+    [styles.compareTo]: v._id === compareTo
+  })
+
+  const articleVersionId = v._id
+  const versionPart = selectedVersion ? `version/${selectedVersion}/` : ''
+  const compareLink  = `/article/${articleId}/${versionPart}compare/${v._id}`
+  const setExportParams = useCallback(() => onExport({ articleId, articleVersionId }), [])
+
+  return <li className={className}>
+    <Link to={`/article/${articleId}/version/${v._id}`}>
+      {v.message ? v.message : 'No label'}
+      ({v.version}.{v.revision})
+    </Link>
+    <p>
+      {v.owner && (
+        <span>
+          by <strong>{v.owner.displayName}</strong>{', '}
+        </span>
+      )}
+      <span>
+        <time dateTime={v.updatedAt}>{formatTimeAgo(v.updatedAt)}</time>
+      </span>
+    </p>
+    <ul className={styles.actions}>
+      {![compareTo, selectedVersion].includes(v._id) && (
+        <li>
+          <Link
+            className={clsx(buttonStyles.button, buttonStyles.secondary)}
+            to={compareLink}
+          >
+            Compare
+          </Link>
+        </li>
+      )}
+      {v._id === compareTo && (
+        <li>
+          <Link
+            className={clsx(buttonStyles.button, buttonStyles.secondary)}
+            to={`/article/${articleId}/${versionPart}`}
+          >
+            Stop
+          </Link>
+        </li>
+      )}
+      <li>
+        <Link
+          to={`/article/${articleId}/version/${v._id}/annotate`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={clsx(buttonStyles.button, buttonStyles.secondary)}
+        >
+          Annotate
+        </Link>
+      </li>
+      <li>
+        <Button onClick={setExportParams}>
+          Export
+        </Button>
+      </li>
+    </ul>
+  </li>
+}
+
+Version.propTypes = {
+  articleId: PropTypes.string.isRequired,
+  v: PropTypes.object.isRequired,
+  selectedVersion: PropTypes.string,
+  compareTo: PropTypes.string,
+  onExport: PropTypes.func.isRequired
+}
 
 export default function Versions ({ article, selectedVersion, compareTo, readOnly }) {
   const articleVersions = useSelector(state => state.articleVersions, shallowEqual)
@@ -27,13 +103,12 @@ export default function Versions ({ article, selectedVersion, compareTo, readOnl
     dispatch({ type: 'ARTICLE_PREFERENCES_TOGGLE', key: 'expandVersions', value: false })
     setExpandCreateForm(true)
   }, [])
+  const handleVersionExport = useCallback(({ articleId, articleVersionId }) => setExportParams({ articleId, articleVersionId }), [])
+  const cancelExport = useCallback(() => setExportParams({}), [])
 
   return (
-    <section className={[styles.section, menuStyles.section].join(' ')}>
-      <h1
-        className={expand ? null : styles.closed}
-        onClick={toggleExpand}
-      >
+    <section className={clsx(styles.section, menuStyles.section)}>
+      <h1 className={expand ? null : styles.closed} onClick={toggleExpand}>
         {expand ? <ChevronDown/> : <ChevronRight/>}
         Versions
 
@@ -42,7 +117,7 @@ export default function Versions ({ article, selectedVersion, compareTo, readOnl
         </Button>
       </h1>
       {exportParams.articleId && (
-        <Modal title="Export" cancel={() => setExportParams({})}>
+        <Modal title="Export" cancel={cancelExport}>
           <Export articleId={exportParams.articleId} articleVersionId={exportParams.articleVersionId} />
         </Modal>
       )}
@@ -52,94 +127,30 @@ export default function Versions ({ article, selectedVersion, compareTo, readOnl
 
           {articleVersions.length === 0 && (<p>
             <strong>All changes are automatically saved.</strong><br/>
-            You should create a new version to record a specific set of changes.
+            Create a new version to keep track of particular changes.
           </p>)}
 
-          {readOnly && <Link className={[buttonStyles.button, buttonStyles.secondary, styles.editMode].join(' ')} to={`/article/${article._id}`}> <ArrowLeft/> Edit Mode</Link>}
+          {readOnly && <Link className={clsx(buttonStyles.button, buttonStyles.secondary, styles.editMode)} to={`/article/${article._id}`}> <ArrowLeft/> Edit Mode</Link>}
 
           <ul className={styles.versionsList}>
             {articleVersions.map((v) => (
-              <li
-                key={`showVersion-${v._id}`}
-                className={
-                  v._id === selectedVersion
-                    ? styles.selected
-                    : v._id === compareTo
-                    ? styles.compareTo
-                    : null
-                }
-              >
-                <Link to={`/article/${article._id}/version/${v._id}`}>
-                  {v.message ? v.message : 'No label'}
-                  ({v.version}.{v.revision})
-                </Link>
-                <p>
-                  {v.owner && (
-                    <span>
-                      by <strong>{v.owner.displayName}</strong>{', '}
-                    </span>
-                  )}
-                  <span>
-                    <time dateTime={v.updatedAt}>{formatTimeAgo(v.updatedAt)}</time>
-                  </span>
-                </p>
-                <ul className={styles.actions}>
-                  {v._id !== compareTo && (
-                    <li>
-                      <Link
-                        className={[buttonStyles.button, buttonStyles.secondary].join(' ')}
-                        to={`/article/${article._id}/${
-                          selectedVersion
-                            ? 'version/' + selectedVersion + '/'
-                            : ''
-                        }compare/${v._id}`}
-                      >
-                        Compare
-                      </Link>
-                    </li>
-                  )}
-                  {v._id === compareTo && (
-                    <li>
-                      <Link
-                        className={[buttonStyles.button, buttonStyles.secondary].join(' ')}
-                        to={`/article/${article._id}/${
-                          selectedVersion
-                            ? 'version/' + selectedVersion
-                            : ''
-                        }`}
-                      >
-                        Stop
-                      </Link>
-                    </li>
-                  )}
-                  <li>
-                    <Link
-                      to={`/article/${article._id}/version/${v._id}/annotate`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className={[buttonStyles.button, buttonStyles.secondary].join(' ')}
-                    >
-                      Annotate
-                    </Link>
-                  </li>
-                  <li>
-                    <Button
-                      onClick={() => {
-                        setExportParams({
-                          articleId: article._id,
-                          articleVersionId: v._id
-                        })
-                      }}
-                    >
-                      Export
-                    </Button>
-                  </li>
-                </ul>
-              </li>
+              <Version key={`showVersion-${v._id}`}
+                articleId={article._id}
+                selectedVersion={selectedVersion}
+                compareTo={compareTo}
+                onExport={handleVersionExport}
+                v={v} />
             ))}
           </ul>
         </>
       )}
     </section>
   )
+}
+
+Versions.propTypes = {
+  article: PropTypes.object.isRequired,
+  selectedVersion: PropTypes.string,
+  compareTo: PropTypes.string,
+  readOnly: PropTypes.bool
 }
