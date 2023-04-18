0b72d3d511b0fa3fa257e0c546233c2c042b3d95
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2023-04-18 12:29:16 +0200

    2.0.16

--- front/vite.config.js
+++ front/vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig, loadEnv } from 'vite'
 import { createRequire } from 'module'
 import process from 'node:process'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
 import handlebars from 'vite-plugin-handlebars'
 import graphql from '@rollup/plugin-graphql'
 
 const require = createRequire(import.meta.url)
-const { version, browserlist: target } = require('./package.json')
+const { version } = require('../package.json')
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
