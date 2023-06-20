ce54f456101545bd8aabcc641f7572225835c01f
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2023-06-20 16:27:05 +0200

    Supprime le mutate inutile

--- front/src/components/corpus/CorpusSelectItem.jsx
+++ front/src/components/corpus/CorpusSelectItem.jsx
@@ -1,21 +1,18 @@
 import React, { useCallback } from 'react'
 import clsx from 'clsx'
 import PropTypes from 'prop-types'
-import { useMutate } from '../../hooks/graphql.js'
 
-import { removeArticleFromCorpus, addArticleToCorpus, getCorpus } from './Corpus.graphql'
+import { removeArticleFromCorpus, addArticleToCorpus } from './Corpus.graphql'
 import styles from './CorpusSelectItem.module.scss'
 import { useGraphQL } from '../../helpers/graphQL.js'
 
 export default function CorpusSelectItem ({ selected, name, id, articleId, onChange }) {
   const runQuery = useGraphQL()
-  const { mutate } = useMutate({ query: getCorpus, variables: { filter: { corpusId: id }, includeArticles: true } })
   const toggleCorpusArticle = useCallback(async (event) => {
     event.preventDefault()
     const [corpusId, checked] = [event.target.value, event.target.checked]
     const query = checked ? addArticleToCorpus : removeArticleFromCorpus
     await runQuery({ query, variables: { articleId: articleId, corpusId: corpusId } })
-    await mutate()
     onChange({corpusId})
   }, [articleId, id, onChange])
   return (
