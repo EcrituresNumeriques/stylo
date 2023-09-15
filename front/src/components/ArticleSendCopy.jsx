7f9d706c9411432ebc006b71d45ffff93c19e331
Guillaume Grossetie <ggrossetie@gmail.com>
2023-09-15 16:42:06 +0200

    Ajout de l'action permettant d'envoyer une copie
    resolves #919

--- front/src/components/ArticleSendCopy.jsx
+++ front/src/components/ArticleSendCopy.jsx
@@ -1,19 +1,37 @@
 import React, { useCallback } from 'react'
 
-import styles from './articleSendCopy.module.scss'
-import ContactSearch from './ContactSearch.jsx'
 import { useToasts } from '@geist-ui/core'
 import { Send } from 'react-feather'
 
+import {
+  duplicateArticle
+} from './Article.graphql'
+
+import styles from './articleSendCopy.module.scss'
+import ContactSearch from './ContactSearch.jsx'
+import {useMutation} from '../hooks/graphql.js'
+
 export default function ArticleSendCopy ({ article }) {
   const { setToast } = useToasts()
+  const mutation = useMutation()
 
   const handleUserUpdated = useCallback(async ({ user, action }) => {
     if (action === 'select' || action === 'unselect') {
-      setToast({
-        text: `Copie de l'article envoyée à ${user.displayName || user.username}.`,
-        type: 'success',
-      })
+      try {
+        await mutation({
+          query: duplicateArticle,
+          variables: {user: null, to: user._id, article: article._id}
+        })
+        setToast({
+          text: `Copie de l'article envoyée à ${user.displayName || user.username}.`,
+          type: 'success',
+        })
+      } catch (err) {
+        setToast({
+          type: 'error',
+          text: `Unable to send a copy of the article: ${err.message}`
+        })
+      }
     }
   }, [article._id])
 
