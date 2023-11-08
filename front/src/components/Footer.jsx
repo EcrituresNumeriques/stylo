5f870853e2be7df3e2ac2d5ce12ad017cbea265e
RochDLY <roch.delannay@gmail.com>
2023-11-08 17:27:01 +0100

    interface en-fr : modification du footer

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Switch, Route, Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 import styles from './header.module.scss'
 import { HelpCircle } from "react-feather";
@@ -9,6 +10,7 @@ function Footer () {
   const dispatch = useDispatch()
   const userHasConsent = useSelector(state => state.userPreferences.trackingConsent)
   const toggleConsent = useCallback(() => dispatch({ type: 'USER_PREFERENCES_TOGGLE', key: 'trackingConsent' }), [])
+  const { t } = useTranslation()
 
   return (<Switch>
     <Route path="*/preview" />
@@ -22,18 +24,18 @@ function Footer () {
             rel="noopener noreferrer"
           >
             <HelpCircle className={styles.linkIcon} size={14} />
-            Documentation
+            {t('footer.documentation.link')}
           </a></li>
           <li>
             <a href="https://github.com/EcrituresNumeriques/stylo/releases" rel="noopener noreferrer" target="_blank">
-              Changelog
+            {t('footer.changelog.link')}
             </a>
           </li>
           <li><Link to="/privacy">Privacy</Link></li>
           { import.meta.env.SNOWPACK_MATOMO_URL && (<li>
             <label className={styles.consentLabel}>
               <input type="checkbox" checked={userHasConsent} onChange={toggleConsent} disabled={true} />
-              I accept to share my navigation stats
+              {t('footer.navStats.checkbox')}
             </label>
           </li>) }
         </ul>
