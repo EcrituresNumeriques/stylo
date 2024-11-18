31670c16ce58cb42f90a351b0d962e3c84b71046
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2024-11-18 21:12:19 +0100

    chore: ESlint flat configuration

---
+++ graphql/eslint.config.js
@@ -0,0 +1,32 @@
+const globals = require('globals')
+const pluginSecurity = require('eslint-plugin-security')
+const pluginJest = require('eslint-plugin-jest')
+const eslint = require('@eslint/js')
+
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: ["**/*.test.js", "**/tests/**.js"],
+    plugins: {
+      jest: pluginJest,
+    },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+
+    rules: {
+      "security/detect-object-injection": ["off"],
+      "security/detect-non-literal-fs-filename": ["off"],
+    },
+  },
+  eslint.configs.recommended,
+  pluginSecurity.configs.recommended
+]
