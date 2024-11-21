8e8e2d651830a4adb1571de98cd51e6a221fd959
Thomas Parisot <thom4parisot@users.noreply.github.com>
2024-11-21 15:55:05 +0100

    chore(sentry): active les sessions utilisateurs dans l'app graphql

--- graphql/helpers/token.js
+++ graphql/helpers/token.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
+const Sentry = require('@sentry/node')
 
 module.exports.createJWTToken = async function createJWTToken ({ email, jwtSecret}) {
   const user = await User.findOne({ email })
@@ -28,14 +29,18 @@ module.exports.populateUserFromJWT = function populateUserFromJWT ({ jwtSecret }
     try {
       req.token = jwt.verify(jwtToken, jwtSecret)
     } catch (error) {
+      Sentry.setUser(null)
+
       res.status(400)
       return res.json(error)
     }
 
     // 2. Fetch associated user, only if not populated by Passport Session before
     if (req.token._id && !req.user) {
-      req.user = await User.findById(req.token._id)
       // question: should we throw an error is the user does not exist?
+      req.user = await User.findById(req.token._id)
+
+      Sentry.setUser(req.user ? { id: req.user?._id } : null)
     }
 
     return next()
