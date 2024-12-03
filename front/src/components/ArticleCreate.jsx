27c98e8a004dd11ea014c8caddaccb7c732fec1a
Guillaume Grossetie <ggrossetie@gmail.com>
2024-12-03 10:13:42 +0100

    chore: ajuste le style des formulaires verticaux (#1126)

--- front/src/components/ArticleCreate.jsx
+++ front/src/components/ArticleCreate.jsx
@@ -75,7 +75,7 @@ export default function ArticleCreate ({ onSubmit }) {
 
   return (
     <section>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <Field
           ref={titleInputRef}
           {...titleBindings}
