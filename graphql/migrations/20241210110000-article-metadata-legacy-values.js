a2f9b08eca18bbd562bd3d456a1ec0c8f6801d3a
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2024-12-13 14:17:53 +0100

    fix: ferme le client/connexion MongoDB lors des migrations

--- graphql/migrations/20241210110000-article-metadata-legacy-values.js
+++ graphql/migrations/20241210110000-article-metadata-legacy-values.js
@@ -3,54 +3,66 @@ function updateLegacyValues(metadata) {
     metadata.publicationDate = metadata.publicationDate.replace(/\//g, '-')
   }
   if (typeof metadata.keywords === 'string') {
-    metadata.keywords = metadata.keywords.split(',').map(word => word.trim())
+    metadata.keywords = metadata.keywords.split(',').map((word) => word.trim())
   }
 }
 
 exports.up = async function (db) {
   const mongo = await db._run('getDbInstance', true)
-  const articles = mongo.collection('articles')
-  const articlesCursor = articles.find({})
-  while (await articlesCursor.hasNext()) {
-    const article = await articlesCursor.next()
-    const metadata = article.workingVersion.metadata
-    if (metadata) {
-      try {
-        updateLegacyValues(metadata)
-      } catch (error) {
-        console.error(`Unable to update legacy values in metadata on article with id: ${article._id} - reason: ${error.reason}`)
-      }
-    }
-    await articles.updateOne({ _id: article._id }, {
-        $set: {
-          'workingVersion.metadata': metadata
+  try {
+    const articles = mongo.collection('articles')
+    const articlesCursor = articles.find({})
+    while (await articlesCursor.hasNext()) {
+      const article = await articlesCursor.next()
+      const metadata = article.workingVersion.metadata
+      if (metadata) {
+        try {
+          updateLegacyValues(metadata)
+        } catch (error) {
+          console.error(
+            `Unable to update legacy values in metadata on article with id: ${article._id} - reason: ${error.reason}`
+          )
         }
-      },
-      { upsert: false }
-    )
-  }
-  await articlesCursor.close()
-  const versions = mongo.collection('versions')
-  const versionsCursor = versions.find({})
-  while (await versionsCursor.hasNext()) {
-    const version = await versionsCursor.next()
-    const metadata = version.metadata
-    if (metadata) {
-      try {
-        updateLegacyValues(metadata)
-      } catch (error) {
-        console.error(`Unable to update legacy values in metadata on version with id: ${version._id} - reason: ${error.reason}`)
       }
+      await articles.updateOne(
+        { _id: article._id },
+        {
+          $set: {
+            'workingVersion.metadata': metadata,
+          },
+        },
+        { upsert: false }
+      )
     }
-    await versions.updateOne({ _id: version._id }, {
-        $set: {
-          metadata
+    await articlesCursor.close()
+    const versions = mongo.collection('versions')
+    const versionsCursor = versions.find({})
+    while (await versionsCursor.hasNext()) {
+      const version = await versionsCursor.next()
+      const metadata = version.metadata
+      if (metadata) {
+        try {
+          updateLegacyValues(metadata)
+        } catch (error) {
+          console.error(
+            `Unable to update legacy values in metadata on version with id: ${version._id} - reason: ${error.reason}`
+          )
         }
-      },
-      { upsert: false }
-    )
+      }
+      await versions.updateOne(
+        { _id: version._id },
+        {
+          $set: {
+            metadata,
+          },
+        },
+        { upsert: false }
+      )
+    }
+    await versionsCursor.close()
+  } finally {
+    mongo.close()
   }
-  await versionsCursor.close()
 }
 
 exports.down = function () {
