fc9c918a623bad01e296b083171c6334a90f0925
Thomas Parisot <thom4parisot@users.noreply.github.com>
2024-12-17 17:21:12 +0100

    feat(front): création de composants Chip et Checkbox

---
+++ front/src/components/Checkbox.jsx
@@ -0,0 +1,53 @@
+import React, { useId } from 'react'
+
+import styles from './Checkbox.module.scss'
+
+/**
+ * @typedef {Object} CheckboxProps
+ * @property {function} onClick
+ * @property {string} name
+ * @property {string} value
+ * @property {string=} color
+ * @property {boolean=} readonly
+ * @property {boolean=} readonly
+ */
+
+/**
+ *
+ * @param {CheckboxProps} props
+ * @returns
+ */
+export default function Checkbox({
+  onChange,
+  name,
+  value,
+  defaultChecked = false,
+  color = null,
+  readonly = false,
+  children,
+}) {
+  const id = `input-checkbox-${useId()}`
+
+  return (
+    <label className={styles.label} htmlFor={id}>
+      <input
+        id={id}
+        name={name}
+        value={value}
+        type="checkbox"
+        defaultChecked={defaultChecked}
+        onChange={onChange}
+        hidden={readonly}
+      />
+      {children}
+
+      {color && (
+        <span
+          className={styles.chip}
+          style={{ backgroundColor: color }}
+          role="presentation"
+        />
+      )}
+    </label>
+  )
+}
