6530eaa2b6631059d76ffc3348fdd880a9c16466
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2025-01-24 10:33:54 +0100

    feat: création d'une molecule `Alert`
    - 4 types : warning, error, info et success
    - avec ou sans icone
    - message personnalisable
    
    Au passage, simplifie la molecule `Loading`.
    Configure la taille de l'icone par rapport à la taille du texte.
    Supprime la notion de `inline` et le padding autour du composant.

---
+++ front/src/components/molecules/Loading.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import clsx from 'clsx'
+import { Loader } from 'react-feather'
+
+import styles from './Loading.module.scss'
+
+/**
+ * @typedef {Object} LoadingProps
+ * @property {string=} label (default: 'Loading…')
+ * @property {boolean=} inline (default: false)
+ * @property {boolean=} hidden (default: false)
+ */
+
+/**
+ * @param {LoadingProps} props
+ * @returns {React.ReactHTMLElement}
+ */
+export default function Loading({
+  label = 'Loading…',
+  size = '1rem',
+  hidden = false,
+}) {
+  return (
+    <div
+      className={clsx(styles.loading)}
+      style={{ fontSize: size }}
+      hidden={hidden}
+    >
+      <Loader className={styles.icon} aria-hidden />
+      {label}
+    </div>
+  )
+}
