c1eb1383a1f317ddeb334afd2cd38eaa24e59141
Thomas Parisot <thom4parisot@users.noreply.github.com>
2025-01-29 11:17:24 +0100

    fix: bump des dépendances qui passent mieux en node@22

--- front/babel.config.cjs
+++ front/babel.config.cjs
@@ -1,16 +1,20 @@
 /* eslint-disable no-undef */
 module.exports = {
-  "presets": [
-    ['@babel/preset-react', {
-      runtime: 'automatic',
-      development: process.env.NODE_ENV === 'development',
-      importSource: '@welldone-software/why-did-you-render',
-    }],
+  presets: [
     [
-      '@babel/preset-env', {
-        corejs: "3.21",
-        useBuiltIns: "entry"
-      }
-    ]
-  ]
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+        development: process.env.NODE_ENV === 'development',
+        importSource: '@welldone-software/why-did-you-render',
+      },
+    ],
+    [
+      '@babel/preset-env',
+      {
+        corejs: '3.38',
+        useBuiltIns: 'entry',
+      },
+    ],
+  ],
 }
