b13170b0f5d21d5465bc286d2070fec10a3cbaf6
Thomas Parisot <thom4parisot@users.noreply.github.com>
2025-02-06 12:26:29 +0100

    feat(routing): passage à react-router@7, redux@5 et react-redux@9

--- front/vite.config.js
+++ front/vite.config.js
@@ -59,11 +59,6 @@ export default defineConfig(async ({ mode }) => {
     define: {
       APP_VERSION: JSON.stringify(pkg.version),
       APP_ENVIRONMENT: JSON.stringify(env.SENTRY_ENVIRONMENT),
-    resolve: {
-      alias: {
-        'react-redux':
-          mode === 'development' ? 'react-redux/lib' : 'react-redux',
-      },
     },
     server: {
       port: 3000,
