e1aa6582ca2d2965959355599bbee4c669817b27
Guillaume Grossetie <ggrossetie@gmail.com>
2025-03-04 16:29:57 +0100

    fix: mise à jour des tags associés à l'article sur la liste des articles

--- front/src/components/Article.jsx
+++ front/src/components/Article.jsx
@@ -184,6 +184,16 @@ export default function Article({
     setSharing(false)
   }, [setSharing])
 
+  const handleArticleTagsUpdated = useCallback(
+    (event) => {
+      onArticleUpdated({
+        ...article,
+        tags: event.updatedTags,
+      })
+    },
+    [article]
+  )
+
   return (
     <article className={styles.article}>
       <Modal
@@ -413,7 +423,11 @@ export default function Article({
               <>
                 <h4>{t('article.tags.title')}</h4>
                 <div className={styles.editTags}>
-                  <ArticleTags articleId={article._id} userTags={userTags} />
+                  <ArticleTags
+                    articleId={article._id}
+                    userTags={userTags}
+                    onArticleTagsUpdated={handleArticleTagsUpdated}
+                  />
                 </div>
               </>
             )}
