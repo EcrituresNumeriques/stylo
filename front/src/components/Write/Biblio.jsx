339c5a7c32aa7281a8b820a7694e8fbac8de7b7a
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2025-03-17 17:51:08 +0100

    chore: ajuste l'utilisation des modales

--- front/src/components/Write/Biblio.jsx
+++ front/src/components/Write/Biblio.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useCallback } from 'react'
+import React, { useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 
 import { ChevronDown, ChevronRight } from 'react-feather'
+import { useModal } from '../../hooks/modal.js'
 
 import Modal from '../Modal'
 import Bibliographe from './bibliographe/Bibliographe'
@@ -15,40 +16,36 @@ import ReferenceList from './ReferenceList'
 function Biblio({ article, readOnly }) {
   const expand = useSelector((state) => state.articlePreferences.expandBiblio)
   const dispatch = useDispatch()
+  const modal = useModal()
   const toggleExpand = useCallback(
     () => dispatch({ type: 'ARTICLE_PREFERENCES_TOGGLE', key: 'expandBiblio' }),
     []
   )
 
-  const [modal, setModal] = useState(false)
-  const openModal = useCallback((event) => {
-    event.preventDefault()
-    event.stopPropagation()
-    setModal(true)
-  }, [])
-  const closeModal = useCallback(() => setModal(false), [])
   const { t } = useTranslation()
 
   return (
     <section className={[menuStyles.section, styles.section].join(' ')}>
       <h1 onClick={toggleExpand}>
-        {expand ? <ChevronDown /> : <ChevronRight />}{' '}
+        {expand ? <ChevronDown /> : <ChevronRight />}
         {t('write.sidebar.biblioTitle')}
         <Button
           className={styles.headingAction}
           small={true}
           disabled={readOnly}
-          onClick={openModal}
+          onClick={(event) => {
+            event.stopPropagation()
+            event.preventDefault()
+            modal.show()
+          }}
         >
           {t('write.sidebar.manageButton')}
         </Button>
       </h1>
       {expand && <ReferenceList />}
-      {modal && (
-        <Modal title={t('write.biblioModal.title')} cancel={closeModal}>
-          <Bibliographe cancel={closeModal} article={article} />
-        </Modal>
-      )}
+      <Modal {...modal.bindings} title={t('write.biblioModal.title')}>
+        <Bibliographe cancel={() => modal.close()} article={article} />
+      </Modal>
     </section>
   )
 }
