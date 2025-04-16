d832d388c521bab0f08e30ede9f6e03b6fab7419
Thomas Parisot <thom4parisot@users.noreply.github.com>
2025-04-16 18:15:17 +0200

    fix: corrige l'initialisation de redux pour les tests sans DevTools

--- front/src/createReduxStore.js
+++ front/src/createReduxStore.js
@@ -508,11 +508,12 @@ function updateSelectedTag(state, { tagId }) {
   }
 }
 
-const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-    trace: true,
-    traceLimit: 25,
-  }) || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      trace: true,
+      traceLimit: 25,
+    })
+  : compose
 
 export default function createReduxStore(state = {}) {
   return createStore(
