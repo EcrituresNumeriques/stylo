14e18b07d5866ccbe0e4144d861bffd71abad059
Thomas Parisot <138627+thom4parisot@users.noreply.github.com>
2025-04-22 11:51:37 +0200

    Update front/src/components/header/UserMenu.jsx

--- front/src/components/header/UserMenu.jsx
+++ front/src/components/header/UserMenu.jsx
@@ -18,7 +18,7 @@ export default function UserMenu() {
 
   const handleLogout = useCallback(() => {
     setIsComponentVisible(false)
-    logout().then()
+    logout()
   }, [])
 
   const { ref, isComponentVisible, setIsComponentVisible } =
