c1b06e3d41be0dbf520725b7ad956c6dab607177
Thomas Parisot <thom4parisot@users.noreply.github.com>
2025-05-16 12:37:16 +0200

    feat: honore le consentement de suivi
    fixes #499
    
    fixes #472

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -2,47 +2,41 @@ import React from 'react'
 import { NavLink } from 'react-router'
 import { useTranslation } from 'react-i18next'
 
-import { usePreferenceItem } from '../hooks/user.js'
-
 import styles from './header.module.scss'
 
 export default function Footer() {
   const { t } = useTranslation()
 
-  const { value: userHasConsent, toggleValue } = usePreferenceItem(
-    'trackingConsent',
-    'user'
-  )
-
   return (
     <footer className={styles.footerContainer}>
       <ul className={styles.footerList}>
-        <li>Stylo {APP_VERSION}</li>
         <li>
           <a
             href="https://github.com/EcrituresNumeriques/stylo/releases"
             rel="noopener noreferrer"
             target="_blank"
+            aria-label={t('footer.changelog.link', { version: APP_VERSION })}
           >
-            {t('footer.changelog.link')}
+            Stylo {APP_VERSION}
           </a>
         </li>
         <li>
           <NavLink to="/privacy">{t('footer.privacy.link')}</NavLink>
         </li>
-        {import.meta.env.SNOWPACK_MATOMO_URL && (
-          <li>
-            <label className={styles.consentLabel}>
-              <input
-                type="checkbox"
-                checked={userHasConsent}
-                onChange={toggleValue}
-                disabled={true}
-              />
-              {t('footer.navStats.checkbox')}
-            </label>
-          </li>
-        )}
+        <li>
+          <a
+            href="https://stylo-doc.ecrituresnumeriques.ca"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {t('footer.documentation.link')}
+          </a>
+        </li>
+        <li>
+          <a href="https://discussions.revue30.org/c/stylo/" target="_blank" rel="noopener noreferrer">
+            {t('footer.community.link')}
+          </a>
+        </li>
       </ul>
     </footer>
   )
