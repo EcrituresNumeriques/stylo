2b3a4ea7ea171cd1ecffc82593f939ed122d9e1a
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2025-05-20 14:03:37 +0200

    chore: ajout des métadonnées pour les thèses

--- front/src/components/corpus/CorpusMetadataModal.jsx
+++ front/src/components/corpus/CorpusMetadataModal.jsx
@@ -1,11 +1,13 @@
-import React, { useCallback, useState } from 'react'
 import { List } from 'lucide-react'
+import React, { useCallback, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch } from 'react-redux'
 import { useGraphQLClient } from '../../helpers/graphQL.js'
 import { useModal } from '../../hooks/modal.js'
-import corpusMetadataSchema from '../../schemas/corpus-journal-metadata.schema.json'
-import corpusUiSchema from '../../schemas/corpus-journal-ui-schema.json'
+import corpusJournalMetadataSchema from '../../schemas/corpus-journal-metadata.schema.json'
+import corpusJournalUiSchema from '../../schemas/corpus-journal-ui-schema.json'
+import corpusThesisUiSchema from '../../schemas/corpus-thesis-ui-schema.json'
+import corpusThesisMetadataSchema from '../../schemas/corpus-thesis-metadata.schema.json'
 import Button from '../Button.jsx'
 import MetadataForm from '../metadata/MetadataForm.jsx'
 import Modal from '../Modal.jsx'
@@ -15,13 +17,16 @@ import Select from '../Select.jsx'
 import { updateMetadata } from './Corpus.graphql'
 import styles from './CorpusMetadataModal.module.scss'
 
-export default function CorpusMetadataModal({ corpusId, initialValue }) {
+export default function CorpusMetadataModal ({ corpusId, initialValue }) {
   const { t } = useTranslation()
   const dispatch = useDispatch()
   const { query } = useGraphQLClient()
   const [corpusMetadata, setCorpusMetadata] = useState(initialValue)
   const modal = useModal()
+  const [metadataType, setMetadataType] = useState('journal')
 
+  const corpusMetadataSchema = useMemo(() => metadataType === 'journal' ? corpusJournalMetadataSchema : corpusThesisMetadataSchema, [metadataType])
+  const corpusUiSchema = useMemo(() => metadataType === 'journal' ? corpusJournalUiSchema : corpusThesisUiSchema, [metadataType])
   const handleUpdateMetadata = useCallback(async () => {
     await query({
       query: updateMetadata,
@@ -47,19 +52,22 @@ export default function CorpusMetadataModal({ corpusId, initialValue }) {
   return (
     <>
       <Button title="Metadata" icon={true} onClick={() => modal.show()}>
-        <List />
+        <List/>
       </Button>
       <Modal
         {...modal.bindings}
         title={
           <>
-            <List /> {t('corpus.metadataModal.title')}
+            <List/> {t('corpus.metadataModal.title')}
           </>
         }
       >
         <div className={styles.type}>
-          <Select alignLabel={false} id="corpus-type" label="Type">
+          <Select alignLabel={false} id="corpus-type" label="Type" value={metadataType} onChange={(event) => {
+            setMetadataType(event.target.value)
+          }}>
             <option value="journal">{t('corpus.type.journal')}</option>
+            <option value="thesis">{t('corpus.type.thesis')}</option>
           </Select>
         </div>
         <MetadataForm
