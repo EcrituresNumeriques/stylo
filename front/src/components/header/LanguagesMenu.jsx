fe11e2d8de44f3cfa273d6046c1303f7db4ef8a6
Guillaume Grossetie <ggrossetie@gmail.com>
2025-06-20 08:30:51 +0200

    fix(css): corrige l'alignement du bouton 'langues' (#1606)

--- front/src/components/header/LanguagesMenu.jsx
+++ front/src/components/header/LanguagesMenu.jsx
@@ -1,8 +1,8 @@
+import { Languages } from 'lucide-react'
 import React, { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
-import useComponentVisible from '../../hooks/componentVisible.js'
 
-import { Languages } from 'lucide-react'
+import useComponentVisible from '../../hooks/componentVisible.js'
 
 import styles from '../header.module.scss'
 
@@ -17,7 +17,12 @@ export default function LanguagesMenu() {
   }, [])
 
   return (
-    <nav ref={ref} className={styles.toggleMenu} aria-labelledby="languages-selection" aria-description={t('header.languagesMenu.description')}>
+    <nav
+      ref={ref}
+      className={styles.languagesMenu}
+      aria-labelledby="languages-selection"
+      aria-description={t('header.languagesMenu.description')}
+    >
       <button
         id="languages-selection"
         aria-expanded={isComponentVisible}
@@ -29,16 +34,40 @@ export default function LanguagesMenu() {
       </button>
 
       {isComponentVisible && (
-        <div className={styles.toggleMenuContainerAlignEnd} id="header-languages-menu">
-          <ul className={styles.toggleMenuList} aria-label={t('header.languagesMenu.list')}>
+        <div
+          className={styles.toggleMenuContainerAlignEnd}
+          id="header-languages-menu"
+        >
+          <ul
+            className={styles.toggleMenuList}
+            aria-label={t('header.languagesMenu.list')}
+          >
             <li>
-              <button lang="en" onClick={handleLanguageChange} aria-pressed={i18n.language === 'en'}>English</button>
+              <button
+                lang="en"
+                onClick={handleLanguageChange}
+                aria-pressed={i18n.language === 'en'}
+              >
+                English
+              </button>
             </li>
             <li>
-              <button lang="fr" onClick={handleLanguageChange} aria-pressed={i18n.language === 'fr'}>Français</button>
+              <button
+                lang="fr"
+                onClick={handleLanguageChange}
+                aria-pressed={i18n.language === 'fr'}
+              >
+                Français
+              </button>
             </li>
             <li>
-              <button lang="es" onClick={handleLanguageChange} aria-pressed={i18n.language === 'es'}>Español</button>
+              <button
+                lang="es"
+                onClick={handleLanguageChange}
+                aria-pressed={i18n.language === 'es'}
+              >
+                Español
+              </button>
             </li>
           </ul>
         </div>
