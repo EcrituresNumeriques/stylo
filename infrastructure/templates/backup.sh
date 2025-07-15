2cf6783e76bd3b2228230696ac3783d8ffe948c0
Guillaume Grossetie <ggrossetie@gmail.com>
2025-07-15 14:47:07 +0200

    chore: l'outil de sauvegarde peut restaurer un fichier avec un Point In Time Date

--- infrastructure/templates/backup.sh
+++ infrastructure/templates/backup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-timestamp=$(date '+%Y%m%d-%H%M%S')
-docker exec mongodb-stylo sh -c 'mongodump --gzip --archive --db {{ mongo_server_db }}' > "{{ mongo_backup_path }}/{{ mongo_server_db }}--${timestamp}.bson.gz"
+docker exec mongodb-stylo sh -c 'mongodump --gzip --archive --db {{ mongo_server_db }}' > "{{ mongo_backup_path }}/{{ mongo_server_db }}.bson.gz"
