3a35114fab7b7d36f79468343e89dc4f386d4281
Guillaume Grossetie <ggrossetie@gmail.com>
2025-07-15 14:43:45 +0200

    feat: configure une sauvegarde quotidienne (#1657)

--- infrastructure/templates/backup.sh
+++ infrastructure/templates/backup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker exec mongodb-stylo sh -c 'mongodump --gzip --archive --db {{ mongo_server_db }}' > {{ mongo_backup_path }}/{{ mongo_server_db }}--{{ app_version }}.bson.gz
+timestamp=$(date '+%Y%m%d-%H%M%S')
+docker exec mongodb-stylo sh -c 'mongodump --gzip --archive --db {{ mongo_server_db }}' > "{{ mongo_backup_path }}/{{ mongo_server_db }}--${timestamp}.bson.gz"
