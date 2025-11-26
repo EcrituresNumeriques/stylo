f8c4b9a4d8e5cda044720351ac4c28df59733500
Thomas Parisot <thom4parisot@users.noreply.github.com>
2025-11-26 16:49:41 +0100

    fix: corrige un nouveau doublon d'import

--- front/src/components/collaborative/CollaborativeEditorWriters.jsx
+++ front/src/components/collaborative/CollaborativeEditorWriters.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
-import { useTranslation } from 'react-i18next'
 
 import { useDisplayName } from '../../hooks/user.js'
 
