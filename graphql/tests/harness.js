33e9966b4e223ccc6d84836a09fc57d539a968cd
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2025-12-08 15:09:50 +0100

    wip

--- graphql/tests/harness.js
+++ graphql/tests/harness.js
@@ -1,10 +1,27 @@
 const { MongoDBContainer } = require('@testcontainers/mongodb')
 const mongoose = require('mongoose')
+const migrate = require('db-migrate')
 
 async function setup() {
-  const container = await new MongoDBContainer('mongo:5.0.31').start()
+  const container = await new MongoDBContainer('mongo:6.0.19').start()
+  const migrateInstance = migrate.getInstance(true, {
+    env: 'dev',
+    config: {
+      dev: {
+        url: container.getConnectionString() + '/stylo-tests',
+        overwrite: {
+          driver: {
+            require: '@ggrossetie/db-migrate-mongodb',
+          },
+        },
+      },
+    },
+  })
+  migrateInstance.silence(false)
+  await migrateInstance.reset()
+  await migrateInstance.up()
   mongoose.set('strictQuery', true)
-  await mongoose.connect(container.getConnectionString(), {
+  await mongoose.connect(container.getConnectionString() + '/stylo-tests', {
     directConnection: true,
   })
   return container
