07ce18ca8838f47d98baaf932b856ded70535b22
Thomas Parisot <thom4parisot@users.noreply.github.com>
2025-12-08 19:30:41 +0100

    feat(editor): smart insertion de liens

--- front/src/components/collaborative/actions/index.test.js
+++ front/src/components/collaborative/actions/index.test.js
@@ -1,6 +1,6 @@
 import { describe, expect, test } from 'vitest'
 
-import { blockAttributes } from './index.js'
+import { blockAttributes, copyPasteUrlFn } from './index.js'
 
 describe('blockAttributes', () => {
   test('works with a single className', () => {
@@ -29,5 +29,23 @@ describe('blockAttributes', () => {
         attrs: null,
       })
     ).toEqual('')
+  })
+})
+
+describe('copyPasteUrlFn()', () => {
+  const cases = [
+    { name: 'nothing/nothing', clipboardText: '', selectionText: '', result: [1, '[](https://example.com)'] },
+    { name: 'text/nothing', clipboardText: 'clip', selectionText: '', result: [6, '[clip](https://example.com)'] },
+    { name: 'nothing/text', clipboardText: '', selectionText: 'text', result: [6, '[text](https://example.com)'] },
+    { name: 'text/text', clipboardText: 'clip', selectionText: 'text', result: [6, '[text](https://example.com)'] },
+    { name: 'url/nothing', clipboardText: 'https://clipboard.local', selectionText: '', result: [1, '[](https://clipboard.local)'] },
+    { name: 'nothing/url', clipboardText: '', selectionText: 'https://selection.local', result: [1, '[](https://selection.local)'] },
+    { name: 'url/url', clipboardText: 'https://clipboard.local', selectionText: 'https://selection.local', result: [25, '[https://selection.local](https://clipboard.local)'] },
+    { name: 'text/url', clipboardText: 'clip', selectionText: 'https://selection.local', result: [6, '[clip](https://selection.local)'] },
+    { name: 'url/text', clipboardText: 'https://clipboard.local', selectionText: 'text', result: [6, '[text](https://clipboard.local)'] },
+  ]
+
+  test.each(cases)('$name', ({ clipboardText, selectionText, result }) => {
+    expect(copyPasteUrlFn({ body_template: '[{{text}}]({{url}})', clipboardText, selectionText })).toEqual(result)
   })
 })
