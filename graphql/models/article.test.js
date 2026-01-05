fbb8872ed1cc71928479afe0b85a50622bc07012
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2026-01-05 10:31:14 +0100

    fix: migre les tests sur les articles

--- graphql/models/article.test.js
+++ graphql/models/article.test.js
@@ -4,7 +4,20 @@ const User = require('./user.js')
 const Version = require('./version.js')
 const Workspace = require('./workspace.js')
 
+const { after, before, describe, test } = require('node:test')
+const assert = require('node:assert')
+const { setup, teardown } = require('../tests/harness.js')
+
 describe('remove', () => {
+  let container
+  before(async () => {
+    container = await setup()
+  })
+
+  after(async () => {
+    await teardown(container)
+  })
+
   test('remove article', async () => {
     // given
     const user = await User.create({
@@ -60,20 +73,20 @@ describe('remove', () => {
     // then...
     // chapter 1 must be removed from workspace
     const w = await Workspace.findOne({ _id: workspace.id })
-    expect(w.articles).toHaveLength(1)
-    expect(w.articles[0].toString()).toStrictEqual(chapter2.id) // chapter 2 _must not_ be removed
+    assert.strictEqual(w.articles.length, 1)
+    assert.strictEqual(w.articles[0].toString(), chapter2.id) // chapter 2 _must not_ be removed
 
     // chapter 1 must be removed from corpus
     const c = await Corpus.findOne({ _id: corpus.id })
-    expect(c.articles).toHaveLength(1)
-    expect(c.articles[0].article.toString()).toStrictEqual(chapter2.id) // chapter 2 _must not_ be removed
+    assert.strictEqual(c.articles.length, 1)
+    assert.strictEqual(c.articles[0].article.toString(), chapter2.id) // chapter 2 _must not_ be removed
 
     // chapter 1 version must be removed
     const chapter1Version = await Version.findById(chapter1VersionId)
-    expect(chapter1Version).toBeNull()
+    assert.strictEqual(chapter1Version, null)
 
     // chapter 2 version _must not_ be removed
     const chapter2Version = await Version.findById(chapter2VersionId)
-    expect(chapter2Version).not.toBeNull()
+    assert.notStrictEqual(chapter2Version, null)
   })
 })
