f62809c740783f10178d46a27cc4b4fd5b9c6953
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2026-01-08 15:57:52 +0100

    fix: met à jour le type de corpus uniquement si le type est manquant

--- graphql/migrations/20250610141000-corpus-type.js
+++ graphql/migrations/20250610141000-corpus-type.js
@@ -2,7 +2,7 @@ exports.up = async function (db) {
   const mongo = db._getDbInstance()
   const corpus = mongo.collection('corpus')
   await corpus.updateMany(
-    { type: { $exists: true } },
+    { type: { $exists: false } },
     {
       $set: {
         type: 'neutral',
