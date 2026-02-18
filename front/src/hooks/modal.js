2479ae5a35d1e4bad1c4848bc5c2c83c7d10e5e2
Thomas Parisot <thom4parisot@users.noreply.github.com>
2026-02-18 15:24:36 +0100

    fix(modal): ne ferme plus la modale lors d'un click externe

--- front/src/hooks/modal.js
+++ front/src/hooks/modal.js
@@ -4,38 +4,25 @@ export function useModal() {
   const ref = useRef(null)
   const [visible, setVisible] = useState(false)
 
-  const onClick = (event) => {
-    if (!event.target.contains(ref.current)) return
-
-    if (
-      event.offsetX < event.target.offsetWidth &&
-      event.offsetY < event.target.offsetHeight
-    ) {
-      // click is on scrollbar
-      return
-    }
-    close()
-  }
-
   function close() {
     ref.current?.close()
     setVisible(false)
     document.body.removeAttribute('data-scrolling')
-    document.removeEventListener('mousedown', onClick)
+    document.body.removeAttribute('inert')
   }
 
   function show() {
     ref.current?.showModal()
     setVisible(true)
+    document.body.setAttribute('inert', 'true')
     document.body.setAttribute('data-scrolling', 'false')
-    document.addEventListener('mousedown', onClick)
   }
 
   return {
     bindings: {
       ref,
       visible,
-      cancel: () => close(),
+      cancel: close,
     },
     show,
     close,
