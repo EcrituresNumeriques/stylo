292fbf065a31074c06dd2da54e523df9bf766f45
Thomas Parisot <thom4parisot@users.noreply.github.com>
2026-02-18 14:04:48 +0100

    fix(editor): supprime le warning pour caractère ambigü

--- front/src/components/organisms/monaco/options.js
+++ front/src/components/organisms/monaco/options.js
@@ -1,3 +1,8 @@
+/**
+ * @import { editor } from 'monaco-editor'
+ */
+
+/** @type {editor.IEditorOptions|editor.IDiffEditorOptions} */
 export default {
   autoClosingBrackets: 'never',
   automaticLayout: true,
@@ -17,6 +22,10 @@ export default {
   scrollBeyondLastLine: false,
   suggest: {
     showWords: false,
+  },
+  unicodeHighlight: {
+    ambiguousCharacters: false,
+    invisibleCharacters: true
   },
   wordWrap: 'on',
   wrappingIndent: 'none',
