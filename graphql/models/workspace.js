7e5f9825c5f9b873e5afc274d453713fac190e39
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2026-06-10 11:39:41 +0200

    refactor(models): déplace les statics et methods de Workspace dans les options du schéma
    Pour des types plus précis (tsserver), bascule la définition des statics et methods dans les options du constructeur Schema.

--- graphql/models/workspace.js
+++ graphql/models/workspace.js
@@ -42,107 +42,101 @@ const workspaceSchema = new Schema(
       ref: 'User',
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    statics: {
+      /**
+       *
+       * @param {import('./user')} user
+       * @returns {mongoose.Collection} workspaces
+       */
+      findByUser: function findWorkspaceByUser(user) {
+        return this.find({ 'members.user': user?._id }).sort([
+          ['updatedAt', -1],
+        ])
+      },
+
+      /**
+       * @returns {mongoose.Collection} workspaces
+       */
+      findByArticleId: function findWorkspaceByArticleId(articleId) {
+        return this.find({ articles: articleId }).sort([['updatedAt', -1]])
+      },
+
+      deleteArticle: async function deleteArticle(workspaceId, articleId) {
+        // remove article from corpuses associated to this workspace
+        await this.model('Corpus').removeArticle(articleId, workspaceId)
+        return this.updateOne(
+          { _id: workspaceId, articles: articleId },
+          {
+            $pull: {
+              articles: articleId,
+            },
+          },
+          { timestamps: false }
+        )
+      },
+
+      addArticle: function addArticle(workspaceId, articleId) {
+        return this.updateOne(
+          { _id: workspaceId, articles: { $nin: [articleId] } },
+          {
+            $push: {
+              articles: articleId,
+            },
+          },
+          { timestamps: false }
+        )
+      },
+
+      getWorkspaceById: function getWorkspaceById(workspaceId, user) {
+        if (user?.admin === true) {
+          return this.findById(workspaceId)
+        }
+
+        return this.findOne({
+          $and: [{ _id: workspaceId }, { 'members.user': user?._id }],
+        })
+      },
+
+      /**
+       * Removes an article from all workspaces where it appears.
+       *
+       * @param articleId article unique identifier
+       * @returns {Promise<import('mongodb').UpdateResult<import('./workspace')>>}
+       */
+      removeArticle: function removeArticle(articleId) {
+        return this.updateMany(
+          { articles: articleId },
+          {
+            $pull: {
+              articles: articleId,
+            },
+          },
+          { timestamps: true }
+        )
+      },
+    },
+    methods: {
+      findMembersByArticle: async function findMembersByArticle(articleId) {
+        const result = await this.aggregate([
+          { $match: { articles: articleId } },
+          // flatten members in order to create a unique set
+          { $unwind: '$members' },
+          { $group: { _id: null, memberIds: { $addToSet: '$members' } } },
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'memberIds',
+              foreignField: '_id',
+              as: 'members',
+            },
+          },
+        ])
+        return result[0].members
+      },
+    },
+  }
 )
-
-/**
- *
- * @param {import('./user')} user
- * @returns {mongoose.Collection} workspaces
- */
-workspaceSchema.statics.findByUser = function findWorkspaceByUser(user) {
-  return this.find({ 'members.user': user?._id }).sort([['updatedAt', -1]])
-}
-
-/**
- * @returns {mongoose.Collection} workspaces
- */
-workspaceSchema.statics.findByArticleId = function findWorkspaceByArticleId(
-  articleId
-) {
-  return this.find({ articles: articleId }).sort([['updatedAt', -1]])
-}
-
-workspaceSchema.methods.findMembersByArticle =
-  async function findMembersByArticle(articleId) {
-    const result = await this.aggregate([
-      { $match: { articles: articleId } },
-      // flatten members in order to create a unique set
-      { $unwind: '$members' },
-      { $group: { _id: null, memberIds: { $addToSet: '$members' } } },
-      {
-        $lookup: {
-          from: 'users',
-          localField: 'memberIds',
-          foreignField: '_id',
-          as: 'members',
-        },
-      },
-    ])
-    return result[0].members
-  }
-
-workspaceSchema.statics.deleteArticle = async function deleteArticle(
-  workspaceId,
-  articleId
-) {
-  // remove article from corpuses associated to this workspace
-  await this.model('Corpus').removeArticle(articleId, workspaceId)
-  return this.updateOne(
-    { _id: workspaceId, articles: articleId },
-    {
-      $pull: {
-        articles: articleId,
-      },
-    },
-    { timestamps: false }
-  )
-}
-
-workspaceSchema.statics.addArticle = function addArticle(
-  workspaceId,
-  articleId
-) {
-  return this.updateOne(
-    { _id: workspaceId, articles: { $nin: [articleId] } },
-    {
-      $push: {
-        articles: articleId,
-      },
-    },
-    { timestamps: false }
-  )
-}
-
-workspaceSchema.statics.getWorkspaceById = function getWorkspaceById(
-  workspaceId,
-  user
-) {
-  if (user?.admin === true) {
-    return this.findById(workspaceId)
-  }
-
-  return this.findOne({
-    $and: [{ _id: workspaceId }, { 'members.user': user?._id }],
-  })
-}
-
-/**
- * Removes an article from all workspaces where it appears.
- *
- * @param articleId article unique identifier
- * @returns {Promise<import('mongodb').UpdateResult<import('./workspace')>>}
- */
-workspaceSchema.statics.removeArticle = function removeArticle(articleId) {
-  return this.updateMany(
-    { articles: articleId },
-    {
-      $pull: {
-        articles: articleId,
-      },
-    },
-    { timestamps: true }
-  )
-}
 
 module.exports = mongoose.model('Workspace', workspaceSchema)
